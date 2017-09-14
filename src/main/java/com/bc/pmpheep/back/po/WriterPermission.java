e25006f642db665d7b7b12516ba4426fe9d060d8
nyz526 <Administrator@PC-201605302045>
2017-09-14 17:07:46 +0800

    Shiro 权限提交

--- src/main/java/com/bc/pmpheep/back/po/WriterPermission.java
+++ src/main/java/com/bc/pmpheep/back/po/WriterPermission.java
@@ -9,114 +9,132 @@ import org.apache.ibatis.type.Alias;
  * WriterPermission实体类
  * 
  * @author 曾庆峰
- *
+ * 
  */
 @Alias("WriterPermission")
 public class WriterPermission implements Serializable {
-	/**
-	 * 主键
-	 */
-	private Long id;
-	/**
-	 * 权限名称
-	 */
-	private String permissionName;
-	/**
-	 * 是否禁用
-	 */
-	private boolean isDisabled;
-	/**
-	 * 备注
-	 */
-	private String note;
-	/**
-	 * 显示顺序
-	 */
-	private Integer sort;
-	/**
-	 * 创建时间
-	 */
-	private Date gmtCreate;
-	/**
-	 * 修改时间
-	 */
-	private Date gmtUpdate;
-	
-	
+    /**
+     * 主键
+     */
+    private Long    id;
+    /**
+     * 权限名称
+     */
+    private String  permissionName;
+    /**
+     * 是否禁用
+     */
+    private boolean isDisabled;
+    /**
+     * 备注
+     */
+    private String  note;
+    /**
+     * 显示顺序
+     */
+    private Integer sort;
+    /**
+     * 创建时间
+     */
+    private Date    gmtCreate;
+    /**
+     * 修改时间
+     */
+    private Date    gmtUpdate;
+    /**
+     * 相对地址
+     */
+    private String  url;
 
-	public WriterPermission() {
-	}
+    public WriterPermission() {
+    }
 
-	public WriterPermission(String permissionName, boolean isDisabled, String note, Integer sort, Date gmtCreate,
-			Date gmtUpdate) {
-		this.permissionName = permissionName;
-		this.isDisabled = isDisabled;
-		this.note = note;
-		this.sort = sort;
-		this.gmtCreate = gmtCreate;
-		this.gmtUpdate = gmtUpdate;
-	}
+    public WriterPermission(String permissionName, boolean isDisabled, String note, Integer sort,
+    Date gmtCreate, Date gmtUpdate, String url) {
+        this.permissionName = permissionName;
+        this.isDisabled = isDisabled;
+        this.note = note;
+        this.sort = sort;
+        this.gmtCreate = gmtCreate;
+        this.gmtUpdate = gmtUpdate;
+        this.url = url;
+    }
 
-	public Long getId() {
-		return id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public String getPermissionName() {
-		return permissionName;
-	}
+    public String getPermissionName() {
+        return permissionName;
+    }
 
-	public void setPermissionName(String permissionName) {
-		this.permissionName = permissionName;
-	}
+    public void setPermissionName(String permissionName) {
+        this.permissionName = permissionName;
+    }
 
-	public boolean isIsDisabled() {
-		return isDisabled;
-	}
+    public boolean isIsDisabled() {
+        return isDisabled;
+    }
 
-	public void setIsDisabled(boolean isDisabled) {
-		this.isDisabled = isDisabled;
-	}
+    public void setIsDisabled(boolean isDisabled) {
+        this.isDisabled = isDisabled;
+    }
 
-	public String getNote() {
-		return note;
-	}
+    public String getNote() {
+        return note;
+    }
 
-	public void setNote(String note) {
-		this.note = note;
-	}
+    public void setNote(String note) {
+        this.note = note;
+    }
 
-	public Integer getSort() {
-		return sort;
-	}
+    public Integer getSort() {
+        return sort;
+    }
 
-	public void setSort(Integer sort) {
-		this.sort = sort;
-	}
+    public void setSort(Integer sort) {
+        this.sort = sort;
+    }
 
-	public Date getGmtCreate() {
-		return gmtCreate;
-	}
+    public Date getGmtCreate() {
+        return gmtCreate;
+    }
 
-	public void setGmtCreate(Date gmtCreate) {
-		this.gmtCreate = gmtCreate;
-	}
+    public void setGmtCreate(Date gmtCreate) {
+        this.gmtCreate = gmtCreate;
+    }
 
-	public Date getGmtUpdate() {
-		return gmtUpdate;
-	}
+    public Date getGmtUpdate() {
+        return gmtUpdate;
+    }
 
-	public void setGmtUpdate(Date gmtUpdate) {
-		this.gmtUpdate = gmtUpdate;
-	}
+    public void setGmtUpdate(Date gmtUpdate) {
+        this.gmtUpdate = gmtUpdate;
+    }
 
-	@Override
-	public String toString() {
-		return "WriterPermission [id=" + id + ", permissionName=" + permissionName + ", idDisabled=" + isDisabled
-				+ ", note=" + note + ", sort=" + sort + ", gmtCreate=" + gmtCreate + ", gmtUpdate=" + gmtUpdate + "]";
-	}
+    /**
+     * @return the url
+     */
+    public String getUrl() {
+        return url;
+    }
+
+    /**
+     * @param url the url to set
+     */
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public String toString() {
+        return "WriterPermission [id=" + id + ", permissionName=" + permissionName
+               + ", idDisabled=" + isDisabled + ", note=" + note + ", sort=" + sort
+               + ", gmtCreate=" + gmtCreate + ", gmtUpdate=" + gmtUpdate + ", url=" + url + "]";
+    }
 
 }
