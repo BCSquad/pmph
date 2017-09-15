d0e9689234a98d1765a2ecc14c2e9e2cb0feb098
wuyun <280530171@qq.com>
2017-09-15 14:28:32 +0800

    CT

--- src/test/java/com/bc/pmpheep/back/service/test/AreaSeviceTest.java
+++ src/test/java/com/bc/pmpheep/back/service/test/AreaSeviceTest.java
@@ -1,9 +1,6 @@
 package com.bc.pmpheep.back.service.test;
+
 import javax.annotation.Resource;
-
-
-
-
 
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -22,25 +19,24 @@ import com.bc.pmpheep.back.service.AreaService;
  */
 public class AreaSeviceTest extends BaseTest {
 	Logger logger = LoggerFactory.getLogger(AreaSeviceTest.class);
-	
+
 	@Resource
 	private AreaService areaService;
-	
-    @Test
-    @Rollback(false) 
-    public void addArea() throws Exception {
-    	Area a=new Area(5L,"测试", 4);
-    	areaService.addArea(a);
-    	logger.info("-----------------------------------新增--------------------------------------------");
-    	logger.info(a.toString());
-    	logger.info("------------------------------------修改-------------------------------------------");
-    	a.setAreaName("ceshiwwwwwwww");
-    	logger.info(areaService.updateAreaById(a).toString());
-    	logger.info("------------------------------------删除-------------------------------------------");
-    	logger.info(areaService.deleteAreaById(a).toString());
-    	logger.info("-----------------------------------查询-------------------------------------------");
-    	logger.info(areaService.getAreaById(new Area(6L)).toString());
-        
-    }
-    
+
+	@Test
+	@Rollback(false)
+	public void addArea() throws Exception {
+		Area a = new Area(5L, "测试", 4);
+		areaService.addArea(a);
+		logger.info("-----------------------------------新增--------------------------------------------");
+		logger.info(a.toString());
+		logger.info("------------------------------------修改-------------------------------------------");
+		a.setAreaName("ceshiwwwwwwww");
+		logger.info(areaService.updateAreaById(a).toString());
+		logger.info("------------------------------------删除-------------------------------------------");
+		logger.info(areaService.deleteAreaById(a).toString());
+		logger.info("-----------------------------------查询-------------------------------------------");
+		logger.info(areaService.getAreaById(new Area(6L)).toString());
+	}
+
 }
