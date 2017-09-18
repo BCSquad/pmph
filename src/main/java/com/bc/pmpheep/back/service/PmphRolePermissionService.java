56fe8531f5aea248fcd808d614d1641b66f7c40a
wuyun <wuyun@wuyun_pc>
2017-09-18 11:04:19 +0800

    ces

--- src/main/java/com/bc/pmpheep/back/service/PmphRolePermissionService.java
+++ src/main/java/com/bc/pmpheep/back/service/PmphRolePermissionService.java
@@ -1,6 +1,5 @@
 package com.bc.pmpheep.back.service;
 
-import com.bc.pmpheep.back.po.PmphGroup;
 import com.bc.pmpheep.back.po.PmphRolePermission;
 import com.bc.pmpheep.service.exception.CheckedServiceException;
 
@@ -11,38 +10,34 @@ import com.bc.pmpheep.service.exception.CheckedServiceException;
  */
 public interface PmphRolePermissionService {
 
-    /**
+	/**
      * 
      * @param PmphRolePermission 实体对象
      * @return 带主键的PmphRolePermission
-     * @throws CheckedServiceException
-     */
-    PmphGroup addPmphRolePermission(PmphRolePermission pmphRolePermission)
-    throws CheckedServiceException;
-
-    /**
-     * 
-     * @param PmphRolePermission 必须包含主键ID
-     * @return PmphRolePermission
      * @throws CheckedServiceException，NullPointerException(主键为空)
      */
-    PmphRolePermission getPmphRolePermissionById(PmphRolePermission pmphRolePermission)
-    throws CheckedServiceException;
+    PmphRolePermission addPmphRolePermission(PmphRolePermission pmphRolePermission) throws CheckedServiceException ;
 
     /**
      * 
-     * @param PmphRolePermission
+     * @param id 
+     * @return PmphRolePermission
+     * @throws CheckedServiceException ，NullPointerException(主键为空)
+     */
+    PmphRolePermission getPmphRolePermissionById(Long id ) throws CheckedServiceException ;
+
+    /**
+     * 
+     * @param id
      * @return 影响行数
      * @throws CheckedServiceException，NullPointerException(主键为空)
      */
-    Integer deletePmphRolePermissionById(PmphRolePermission pmphRolePermission)
-    throws CheckedServiceException;
+    Integer deletePmphRolePermissionById(Long id) throws CheckedServiceException ;
 
     /**
      * @param PmphRolePermission
      * @return 影响行数
      * @throws CheckedServiceException ，NullPointerException(主键为空)
      */
-    Integer updatePmphRolePermissionById(PmphRolePermission pmphRolePermission)
-    throws CheckedServiceException;
+    Integer updatePmphRolePermission(PmphRolePermission pmphRolePermission) throws CheckedServiceException ;
 }
