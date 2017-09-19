69fe9914e6a00bbd22f3040f6acd72e6a52bd1d5
zqf050217 <791038935@qq.com>
2017-09-19 17:59:33 +0800

    11021

--- src/main/java/com/bc/pmpheep/back/dao/AreaDao.java
+++ src/main/java/com/bc/pmpheep/back/dao/AreaDao.java
@@ -15,38 +15,42 @@ import com.bc.pmpheep.back.po.Area;
 @Repository
 public interface AreaDao {
 
-    /**
-     * 新增一个Area
-     * 
-     * @param area 实体对象
-     * @return 影响行数
-     */
-    Integer addArea(Area area);
+	/**
+	 * 新增一个Area
+	 * 
+	 * @param area
+	 *            实体对象
+	 * @return 影响行数
+	 */
+	Integer addArea(Area area);
 
-    /**
-     * 删除Area 通过主键id
-     * 
-     * @param area
-     * @return 影响行数
-     */
-    Integer deleteAreaById(Long id);
+	/**
+	 * 删除Area 通过主键id
+	 * 
+	 * @param area
+	 * @return 影响行数
+	 */
+	Integer deleteAreaById(Long id);
 
-    /**
-     * 更新一个 Area通过主键id
-     * 
-     * @param area
-     * @return 影响行数
-     */
-    Integer updateArea(Area area);
+	/**
+	 * 更新一个 Area通过主键id
+	 * 
+	 * @param area
+	 * @return 影响行数
+	 */
+	Integer updateArea(Area area);
 
-    /**
-     * 查询一个 Area 通过主键id
-     * 
-     * @param area 必须包含主键ID
-     * @return area
-     */
-    Area getAreaById(Long id);
-    //测试
-    List<Area>   getTest(PageTest<Area>  p);
-    Integer getTestTotal(PageTest<Area>  p);
+	/**
+	 * 查询一个 Area 通过主键id
+	 * 
+	 * @param area
+	 *            必须包含主键ID
+	 * @return area
+	 */
+	Area getAreaById(Long id);
+
+	// 测试
+	List<Area> getTest(PageTest<Area> p);
+
+	Integer getTestTotal(PageTest<Area> p);
 }
