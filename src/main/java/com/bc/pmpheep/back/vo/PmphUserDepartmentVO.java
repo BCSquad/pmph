30a1ca3b2d7fee1c7b09dd16a6667c62949a5aa7
zqf050217 <791038935@qq.com>
2017-09-22 15:14:38 +0800

    添加社内部门VO的path字段用户查询该部门下的所有用户

--- src/main/java/com/bc/pmpheep/back/vo/PmphUserDepartmentVO.java
+++ src/main/java/com/bc/pmpheep/back/vo/PmphUserDepartmentVO.java
@@ -15,59 +15,63 @@ import java.util.List;
  * 
  */
 public class PmphUserDepartmentVO implements Serializable {
-    /**
-     * 主键
-     */
-    private Long                       id;
-    /**
-     * 上级部门id
-     */
-    private Long                       parentId;
-    /**
-     * 部门名称
-     */
-    private String                     dpName;
-    /**
-     * 下级部门
-     */
-    private List<PmphUserDepartmentVO> sonDepartment;
+	/**
+	 * 主键
+	 */
+	private Long id;
+	/**
+	 * 上级部门id
+	 */
+	private Long parentId;
+	/**
+	 * 根节点路径
+	 */
+	private String path;
+	/**
+	 * 部门名称
+	 */
+	private String dpName;
+	/**
+	 * 下级部门
+	 */
+	private List<PmphUserDepartmentVO> sonDepartment;
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public Long getParentId() {
-        return parentId;
-    }
+	public Long getParentId() {
+		return parentId;
+	}
 
-    public void setParentId(Long parentId) {
-        this.parentId = parentId;
-    }
+	public void setParentId(Long parentId) {
+		this.parentId = parentId;
+	}
 
-    public String getDpName() {
-        return dpName;
-    }
+	public String getDpName() {
+		return dpName;
+	}
 
-    public void setDpName(String dpName) {
-        this.dpName = dpName;
-    }
+	public void setDpName(String dpName) {
+		this.dpName = dpName;
+	}
 
-    public List<PmphUserDepartmentVO> getSonDepartment() {
-        return sonDepartment;
-    }
+	public List<PmphUserDepartmentVO> getSonDepartment() {
+		return sonDepartment;
+	}
 
-    public void setSonDepartment(List<PmphUserDepartmentVO> sonDepartment) {
-        this.sonDepartment = sonDepartment;
-    }
+	public void setSonDepartment(List<PmphUserDepartmentVO> sonDepartment) {
+		this.sonDepartment = sonDepartment;
+	}
 
-    @Override
-    public String toString() {
-        return "PmphUserDepartmentVO [id=" + id + ", parentId=" + parentId + ", dpName=" + dpName
-               + ", sonDepartment=" + sonDepartment + "]";
-    }
+	@Override
+	public String toString() {
+		return "PmphUserDepartmentVO [id=" + id + ", parentId=" + parentId + ", dpName=" + dpName + ", sonDepartment="
+				+ sonDepartment + "]";
+	}
 
 }
