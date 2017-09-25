c43cc7709e9bc809de0884444596277bc73323ec
yangliang <280530171@qq.com>
2017-09-25 09:50:39 +0800

    测试块 修改成断言

--- src/test/java/com/bc/pmpheep/back/service/test/WriterUserCertificationServiceTest.java
+++ src/test/java/com/bc/pmpheep/back/service/test/WriterUserCertificationServiceTest.java
@@ -1,10 +1,14 @@
 package com.bc.pmpheep.back.service.test;
 import java.util.Random;
+
 import javax.annotation.Resource;
+
+import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.test.annotation.Rollback;
+
 import com.bc.pmpheep.back.po.WriterUserCertification;
 import com.bc.pmpheep.test.BaseTest;
 import com.bc.pmpheep.back.service.WriterUserCertificationService;
@@ -18,25 +22,25 @@ public class WriterUserCertificationServiceTest extends BaseTest {
 	Logger logger = LoggerFactory.getLogger(WriterUserCertificationServiceTest.class);
 	
 	@Resource
-	private WriterUserCertificationService testService;
+	private WriterUserCertificationService writerUserCertificationService;
 	
     @Test
     @Rollback(Const.ISROLLBACK) 
     public void test() throws Exception {
-    	Random r =new Random();
-    	WriterUserCertification testPar=new WriterUserCertification (new Long(r.nextInt(200)),new Long(r.nextInt(200)),
-    			String.valueOf(r.nextInt(200)), "512345678901111111", new Short("1"), "jjj", null, null) ;
+    	Random random =new Random();
+    	WriterUserCertification writerUserCertification=new WriterUserCertification (new Long(random.nextInt(200)),new Long(random.nextInt(200)),
+    			String.valueOf(random.nextInt(200)), "512345678901111111", new Short("1"), "jjj", null, null) ;
     	logger.info("---WriterUserCertificationService测试---------------------------------------------------------------------------------");
     	//新增
-    	testService.addWriterUserCertification(testPar);
-    	logger.info(testPar.toString());
+    	writerUserCertificationService.addWriterUserCertification(writerUserCertification);
+    	Assert.assertTrue("添加失败",writerUserCertification.getId() > 0 );
     	//修改
-    	testPar.setIdcard(String.valueOf(r.nextInt(200)));
-    	logger.info(testService.updateWriterUserCertification(testPar).toString());
+    	writerUserCertification.setIdcard(String.valueOf(random.nextInt(200)));
+    	Assert.assertTrue("更新失败", writerUserCertificationService.updateWriterUserCertification(writerUserCertification) > 0 );
     	//删除
-    	logger.info(testService.deleteWriterUserCertificationById(2L).toString());
+    	Assert.assertTrue("删除失败", writerUserCertificationService.deleteWriterUserCertificationById(2L) >=0);
     	//查询
-    	logger.info(testService.getWriterUserCertificationById(3L).toString());
+    	Assert.assertNotNull("获取数据失败",writerUserCertificationService.getWriterUserCertificationById(3L));
     	
     }
     
--- src/test/java/com/bc/pmpheep/back/service/test/WriterUserMessageServiceTest.java
+++ src/test/java/com/bc/pmpheep/back/service/test/WriterUserMessageServiceTest.java
@@ -2,6 +2,7 @@ package com.bc.pmpheep.back.service.test;
 
 import javax.annotation.Resource;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,16 +22,8 @@ public class WriterUserMessageServiceTest extends BaseTest {
 		WriterUserMessage writerUserMessage = new WriterUserMessage();
 		writerUserMessage.setMsgId(123L);
 		writerUserMessage.setUserId(1L);
-		try {
-			writerUserMessage = writerUserMessageService.addWriterUserMessage(writerUserMessage);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		if (null != writerUserMessage) {
-			logger.info("添加了{}", writerUserMessage.toString());
-		} else {
-			logger.info("失败了");
-		}
+		writerUserMessage = writerUserMessageService.addWriterUserMessage(writerUserMessage);
+		Assert.assertTrue("失败了",writerUserMessage.getId()>0 );
 
 	}
 
@@ -40,17 +33,9 @@ public class WriterUserMessageServiceTest extends BaseTest {
 		WriterUserMessage writerUserMessage = new WriterUserMessage();
 		writerUserMessage.setMsgId(123L);
 		writerUserMessage.setUserId(1L);
-		try {
-			writerUserMessage = writerUserMessageService.addWriterUserMessage(writerUserMessage);
-			num = writerUserMessageService.deleteWriterUserMessageById(writerUserMessage);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		if (-1 != num) {
-			logger.info("成功删除了{}条数据", num);
-		} else {
-			logger.info("失败了");
-		}
+		writerUserMessage = writerUserMessageService.addWriterUserMessage(writerUserMessage);
+		num = writerUserMessageService.deleteWriterUserMessageById(writerUserMessage.getId());
+		Assert.assertTrue("失败了",num >=0);
 	}
 
 	@Test
@@ -63,18 +48,10 @@ public class WriterUserMessageServiceTest extends BaseTest {
 		writerUserMessage2.setIsDeleted(true);
 		writerUserMessage2.setMsgId(321L);
 		writerUserMessage2.setUserId(2L);
-		try {
-			writerUserMessage = writerUserMessageService.addWriterUserMessage(writerUserMessage);
-			writerUserMessage2.setId(writerUserMessage.getId());
-			num = writerUserMessageService.updateWriterUserMessageById(writerUserMessage2);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		if (null != writerUserMessage) {
-			logger.info("成功修改了{}条数据", num);
-		} else {
-			logger.info("失败了");
-		}
+		writerUserMessage = writerUserMessageService.addWriterUserMessage(writerUserMessage);
+		writerUserMessage2.setId(writerUserMessage.getId());
+		num = writerUserMessageService.updateWriterUserMessage(writerUserMessage2);
+		Assert.assertTrue("失败了",num>0);
 
 	}
 
@@ -84,18 +61,10 @@ public class WriterUserMessageServiceTest extends BaseTest {
 		writerUserMessage.setMsgId(123L);
 		writerUserMessage.setUserId(1L);
 		WriterUserMessage writerUserMessage2 = new WriterUserMessage();
-		try {
-			writerUserMessage = writerUserMessageService.addWriterUserMessage(writerUserMessage);
-			writerUserMessage2.setId(writerUserMessage.getId());
-			writerUserMessage2 = writerUserMessageService.getWriterUserMessageById(writerUserMessage2);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		if (null != writerUserMessage) {
-			logger.info("找到了{}", writerUserMessage.toString());
-		} else {
-			logger.info("失败了");
-		}
+		writerUserMessage = writerUserMessageService.addWriterUserMessage(writerUserMessage);
+		writerUserMessage2.setId(writerUserMessage.getId());
+		writerUserMessage2 = writerUserMessageService.getWriterUserMessageById(writerUserMessage2.getId());
+		Assert.assertTrue("失败了",null != writerUserMessage);
 
 	}
 }
