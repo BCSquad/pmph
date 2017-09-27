eb2caacbe4907a5b41a92299f742dbe2d4be2d5a
yangliang <280530171@qq.com>
2017-09-27 11:44:02 +0800

    spring-websocket 整合

---
+++ src/main/java/com/bc/pmpheep/websocket/MyWebSocketConfig.java
@@ -0,0 +1,46 @@
+package com.bc.pmpheep.websocket;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+/**
+ * 
+ * websocket 配置类
+ *
+ * @author Mryang
+ *
+ * @createDate 2017年9月27日 上午10:13:25
+ *
+ */
+@Configuration
+@EnableWebMvc
+@EnableWebSocket
+public class MyWebSocketConfig extends WebMvcConfigurerAdapter implements WebSocketConfigurer {
+		
+		@Autowired
+		private MyWebSocketHandler handler;
+		
+		@Override
+        public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        	//前台 可以使用websocket环境
+            registry.addHandler(handler,"/websocket").addInterceptors(new HandshakeInterceptor()).setAllowedOrigins("*");
+            //前台 不可以使用websocket环境，则使用sockjs进行模拟连接
+            registry.addHandler(handler, "/sockjs/websocket").addInterceptors(new HandshakeInterceptor()).setAllowedOrigins("*").withSockJS();
+        }
+}
+
+
+
+
+
+
+
+
+
+
+
