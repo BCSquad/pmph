851f283e38474c33ccc5d45a119c42cb290e2db7
罗旭 <gugia@qq.com>
2017-09-27 23:47:04 +0800

    通过消息服务单元测试

--- src/test/java/com/bc/pmpheep/test/HowHandleVoidMethodJUnit.java
+++
@@ -1,9 +0,0 @@
-package com.bc.pmpheep.test;
-
-public class HowHandleVoidMethodJUnit {
-    private String name = "returnVoidMethodTest";
-
-    public void voidMethod() {
-        this.name = "一个返回void的方法如何进行测试???";
-    }
-}
