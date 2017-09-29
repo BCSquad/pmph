d031469d68725e11126f89f8b632c53eedf34b7c
zqf050217 <791038935@qq.com>
2017-09-29 15:13:40 +0800

    添加根据小组id获取小组成员

--- src/main/java/com/bc/pmpheep/back/service/PmphGroupMemberService.java
+++ src/main/java/com/bc/pmpheep/back/service/PmphGroupMemberService.java
@@ -1,42 +1,59 @@
 package com.bc.pmpheep.back.service;
 
+import java.util.List;
+
 import com.bc.pmpheep.back.po.PmphGroupMember;
+import com.bc.pmpheep.back.vo.PmphGroupMemberVO;
 import com.bc.pmpheep.service.exception.CheckedServiceException;
 
 /**
  * PmphGroupMemberService 接口
+ * 
  * @author Mryang
  */
 public interface PmphGroupMemberService {
 
 	/**
 	 * 
-	 * @param  pmphGroupMember 实体对象
-	 * @return  带主键的 PmphGroupMember
-	 * @throws CheckedServiceException 
+	 * @param pmphGroupMember
+	 *            实体对象
+	 * @return 带主键的 PmphGroupMember
+	 * @throws CheckedServiceException
 	 */
-	PmphGroupMember addPmphGroupMember (PmphGroupMember pmphGroupMember) throws CheckedServiceException;
-	
+	PmphGroupMember addPmphGroupMember(PmphGroupMember pmphGroupMember) throws CheckedServiceException;
+
 	/**
 	 * 
 	 * @param 主键id
-	 * @return  PmphGroupMember
+	 * @return PmphGroupMember
 	 * @throws CheckedServiceException
 	 */
 	PmphGroupMember getPmphGroupMemberById(Long id) throws CheckedServiceException;
-	
+
 	/**
 	 * 
 	 * @param 主键id
-	 * @return  影响行数
+	 * @return 影响行数
 	 * @throws CheckedServiceException
 	 */
 	Integer deletePmphGroupMemberById(Long id) throws CheckedServiceException;
-	
+
 	/**
 	 * @param pmphGroupMember
 	 * @return 影响行数
 	 * @throws CheckedServiceException
 	 */
 	Integer updatePmphGroupMember(PmphGroupMember pmphGroupMember) throws CheckedServiceException;
+
+	/**
+	 * 
+	 * 
+	 * 功能描述：根据小组id加载小组用户
+	 *
+	 * @param groupId
+	 *            小组id
+	 * @return 小组成员
+	 *
+	 */
+	List<PmphGroupMemberVO> getListPmphGroupMember(Long groupId) throws CheckedServiceException;
 }
