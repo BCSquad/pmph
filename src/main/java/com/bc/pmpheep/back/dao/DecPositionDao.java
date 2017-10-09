f55ba67675fd399e014e20d30c37d1aea4194cdd
lyc <lyc@netfang.cn>
2017-10-09 16:59:59 +0800

    作家申报职位表 数据访问层接口 by lyc

---
+++ src/main/java/com/bc/pmpheep/back/dao/DecPositionDao.java
@@ -0,0 +1,80 @@
+/**
+ * 
+ */
+package com.bc.pmpheep.back.dao;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.bc.pmpheep.back.po.DecPosition;
+
+/**
+ * <p>Title:作家申报职位表实体类<p>
+ * <p>Description:作家申报职位表 数据访问层接口<p>
+ * @author lyc
+ * @date 2017年10月9日 下午4:42:32
+ */
+
+@Repository
+public interface DecPositionDao {
+    /**
+     * 
+     * Description:新增一个作家申报职位信息
+     * @author:lyc
+     * @date:2017年10月9日下午4:45:24
+     * @Param:
+     * @Return:Integer
+     */
+	Integer addDecPosition (DecPosition decPosition);
+	
+	/**
+	 * 
+	 * Description:根据id删除一个作家申报职位信息
+	 * @author:lyc
+	 * @date:2017年10月9日下午4:48:01
+	 * @Param:
+	 * @Return:Integer
+	 */
+	Integer deleteDecPosition(Long id);
+	
+	/**
+	 * 
+	 * Description:更新作家申报职位信息
+	 * @author:lyc
+	 * @date:2017年10月9日下午4:48:46
+	 * @Param:
+	 * @Return:Integer
+	 */
+	Integer updateDecPosition(DecPosition decPosition);
+	
+	/**
+	 * 
+	 * Description:根据id查询一个作家申报职位信息
+	 * @author:lyc
+	 * @date:2017年10月9日下午4:52:40
+	 * @Param:
+	 * @Return:DecPosition
+	 */
+	DecPosition getDecPositionById(Long id);
+	
+	/**
+	 * 
+	 * Description:根据申报表id查询申报职位信息
+	 * @author:lyc
+	 * @date:2017年10月9日下午4:54:30
+	 * @Param:
+	 * @Return:List<DecPosition>
+	 */
+	List<DecPosition> getListDecPositions(Long declarationId);
+	
+	/**
+	 * 
+	 * Description:查询表的总记录数
+	 * @author:lyc
+	 * @date:2017年10月9日下午4:55:26
+	 * @Param:
+	 * @Return:Long
+	 */
+	Long getDecPosition();
+}
