0f5c5a9fe24bff14c736761aec1ce016cb7a170a
nyz526 <nyz526@163.com>
2017-10-12 11:13:44 +0800

    修改解决axios（POST，PUT，DELETE）请求方式跨域问题

--- src/main/java/com/bc/pmpheep/back/interceptor/ProcessInterceptor.java
+++ src/main/java/com/bc/pmpheep/back/interceptor/ProcessInterceptor.java
@@ -6,6 +6,23 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+/**
+ * 
+ * <pre>
+ * 功能描述：解决axios（POST，PUT，DELETE）请求方式跨域问题
+ * 使用示范：
+ * 
+ * 
+ * @author (作者) nyz
+ * 
+ * @since (该版本支持的JDK版本) ：JDK 1.6或以上
+ * @version (版本) 1.0
+ * @date (开发日期) 2017-10-12
+ * @modify (最后修改时间) 
+ * @修改人 ：nyz 
+ * @审核人 ：
+ * </pre>
+ */
 public class ProcessInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest httpServletRequest,
@@ -26,9 +43,7 @@ public class ProcessInterceptor implements HandlerInterceptor {
             httpServletResponse.setStatus(200);
             return false;
         }
-
-        System.out.println(method);
-
+        // System.out.println(method);
         return true;
     }
 
