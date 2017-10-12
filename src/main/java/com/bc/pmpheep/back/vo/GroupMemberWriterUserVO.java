96a36b2422c8da8515bd461f8da89e2a39b10a9c
lyc <513760648@qq.com>
2017-10-12 19:10:03 +0800

    增加VO属性  by lyc

--- src/main/java/com/bc/pmpheep/back/vo/GroupMemberWriterUserVO.java
+++ src/main/java/com/bc/pmpheep/back/vo/GroupMemberWriterUserVO.java
@@ -18,12 +18,18 @@ import org.apache.ibatis.type.Alias;
 public class GroupMemberWriterUserVO implements Serializable{
 	//主键
     private Long id;
+    //教材书籍id
+    private Long bookId;
     //姓名
     private String realname;
     //账号
     private String username;
+    //教材书籍
+    private String bookname;
     //遴选职务
-    private String chosenPosition;
+    private Integer chosenPosition;
+    //职务名称
+    private String chosenPositionName;
     //工作单位机构id
     private String workId;
     //工作单位
@@ -39,6 +45,14 @@ public class GroupMemberWriterUserVO implements Serializable{
 
 	public void setId(Long id) {
 		this.id = id;
+	}
+
+	public Long getBookId() {
+		return bookId;
+	}
+
+	public void setBookId(Long bookId) {
+		this.bookId = bookId;
 	}
 
 	public String getRealname() {
@@ -57,12 +71,28 @@ public class GroupMemberWriterUserVO implements Serializable{
 		this.username = username;
 	}
 
-	public String getChosenPosition() {
+	public String getBookname() {
+		return bookname;
+	}
+
+	public void setBookname(String bookname) {
+		this.bookname = bookname;
+	}
+
+	public Integer getChosenPosition() {
 		return chosenPosition;
 	}
 
-	public void setChosenPosition(String chosenPosition) {
+	public void setChosenPosition(Integer chosenPosition) {
 		this.chosenPosition = chosenPosition;
+	}
+
+	public String getChosenPositionName() {
+		return chosenPositionName;
+	}
+
+	public void setChosenPositionName(String chosenPositionName) {
+		this.chosenPositionName = chosenPositionName;
 	}
 
 	public String getWorkId() {
@@ -83,9 +113,10 @@ public class GroupMemberWriterUserVO implements Serializable{
 
 	@Override
 	public String toString() {
-		return " {id:" + id + ", realname:" + realname + ", username:"
-				+ username + ", chosenPosition:" + chosenPosition + ", workId:"
-				+ workId + ", workName:" + workName + "}";
+		return " {id:" + id + ", bookId:" + bookId + ", realname:" + realname + ", username:"
+				+ username + ", bookname:" + bookname + ", chosenPosition:" + chosenPosition + ","
+				+ "chosenPositionName:" + chosenPositionName + ", workId:" + workId + ", workName:" 
+				+ workName + "}";
 	}
      
 }
