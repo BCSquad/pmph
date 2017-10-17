2ae76468d1cf5e6d335a47698a4d6b2bd06de33f
罗旭 <gugia@qq.com>
2017-10-18 04:14:01 +0800

    数据迁移示例（小组）

--- src/test/java/com/bc/pmpheep/migration/test/GroupMigrationHelperTest.java
+++ src/test/java/com/bc/pmpheep/migration/test/GroupMigrationHelperTest.java
@@ -6,12 +6,10 @@ package com.bc.pmpheep.migration.test;
 
 import com.bc.pmpheep.migration.GroupMigrationHelper;
 import com.bc.pmpheep.test.BaseTest;
-import com.bc.pmpheep.utils.test.ZipHelperTest;
 import javax.annotation.Resource;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.test.annotation.Rollback;
 
 /**
  * 小组数据迁移工具测试类
@@ -26,7 +24,7 @@ public class GroupMigrationHelperTest extends BaseTest {
     GroupMigrationHelper helper;
 
     @Test
-    @Rollback(false)
+    //@Rollback(false)
     public void demo() {
         helper.group();
         helper.groupMember();
