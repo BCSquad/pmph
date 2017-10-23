c3dd1c35cb2681b436d2685aac64a83afeb90447
zqf050217 <791038935@qq.com>
2017-10-23 15:49:47 +0800

    添加机构创建时间

--- src/main/java/com/bc/pmpheep/back/vo/OrgVO.java
+++ src/main/java/com/bc/pmpheep/back/vo/OrgVO.java
@@ -1,6 +1,7 @@
 package com.bc.pmpheep.back.vo;
 
 import java.io.Serializable;
+import java.sql.Timestamp;
 
 import org.apache.ibatis.type.Alias;
 
@@ -12,151 +13,154 @@ import org.apache.ibatis.type.Alias;
 @SuppressWarnings("serial")
 @Alias("OrgVO")
 public class OrgVO implements Serializable {
-    // 主键
-    private String  id;
-    // 机构名称
-    private String  orgName;
-    // 机构类型id
-    private String  orgTypeId;
-    // 机构类型名称
-    private String  orgTypeName;
-    // 上级机构id
-    private String  parentId;
-    // 区域id
-    private String  areaId;
-    // 所属区域名称
-    private String  areaName;
-    // 联系人
-    private String  countactPerson;
-    // 联系电话
-    private String  countactPhone;
-    // 备注
-    private String  note;
-    // 显示顺序
-    private Integer sort;
-    // 条件分页总条数
-    private Integer count;
+	// 主键
+	private String id;
+	// 机构名称
+	private String orgName;
+	// 机构类型id
+	private String orgTypeId;
+	// 机构类型名称
+	private String orgTypeName;
+	// 上级机构id
+	private String parentId;
+	// 区域id
+	private String areaId;
+	// 所属区域名称
+	private String areaName;
+	// 联系人
+	private String countactPerson;
+	// 联系电话
+	private String countactPhone;
+	// 备注
+	private String note;
+	// 显示顺序
+	private Integer sort;
+	// 条件分页总条数
+	private Integer count;
+	// 创建时间
+	private Timestamp gmtCreate;
 
-    public OrgVO() {
-        super();
-    }
+	public OrgVO() {
+		super();
+	}
 
-    public String getId() {
-        return id;
-    }
+	public String getId() {
+		return id;
+	}
 
-    public void setId(String id) {
-        this.id = id;
-    }
+	public void setId(String id) {
+		this.id = id;
+	}
 
-    public String getOrgName() {
-        return orgName;
-    }
+	public String getOrgName() {
+		return orgName;
+	}
 
-    public void setOrgName(String orgName) {
-        this.orgName = orgName;
-    }
+	public void setOrgName(String orgName) {
+		this.orgName = orgName;
+	}
 
-    public String getOrgTypeId() {
-        return orgTypeId;
-    }
+	public String getOrgTypeId() {
+		return orgTypeId;
+	}
 
-    public void setOrgTypeId(String orgTypeId) {
-        this.orgTypeId = orgTypeId;
-    }
+	public void setOrgTypeId(String orgTypeId) {
+		this.orgTypeId = orgTypeId;
+	}
 
-    public String getOrgTypeName() {
-        return orgTypeName;
-    }
+	public String getOrgTypeName() {
+		return orgTypeName;
+	}
 
-    public void setOrgTypeName(String orgTypeName) {
-        this.orgTypeName = orgTypeName;
-    }
+	public void setOrgTypeName(String orgTypeName) {
+		this.orgTypeName = orgTypeName;
+	}
 
-    public String getAreaId() {
-        return areaId;
-    }
+	public String getAreaId() {
+		return areaId;
+	}
 
-    public void setAreaId(String areaId) {
-        this.areaId = areaId;
-    }
+	public void setAreaId(String areaId) {
+		this.areaId = areaId;
+	}
 
-    public String getAreaName() {
-        return areaName;
-    }
+	public String getAreaName() {
+		return areaName;
+	}
 
-    public void setAreaName(String areaName) {
-        this.areaName = areaName;
-    }
+	public void setAreaName(String areaName) {
+		this.areaName = areaName;
+	}
 
-    public String getCountactPerson() {
-        return countactPerson;
-    }
+	public String getCountactPerson() {
+		return countactPerson;
+	}
 
-    public void setCountactPerson(String countactPerson) {
-        this.countactPerson = countactPerson;
-    }
+	public void setCountactPerson(String countactPerson) {
+		this.countactPerson = countactPerson;
+	}
 
-    public String getCountactPhone() {
-        return countactPhone;
-    }
+	public String getCountactPhone() {
+		return countactPhone;
+	}
 
-    public void setCountactPhone(String countactPhone) {
-        this.countactPhone = countactPhone;
-    }
+	public void setCountactPhone(String countactPhone) {
+		this.countactPhone = countactPhone;
+	}
 
-    public String getNote() {
-        return note;
-    }
+	public String getNote() {
+		return note;
+	}
 
-    public void setNote(String note) {
-        this.note = note;
-    }
+	public void setNote(String note) {
+		this.note = note;
+	}
 
-    public Integer getSort() {
-        return sort;
-    }
+	public Integer getSort() {
+		return sort;
+	}
 
-    public void setSort(Integer sort) {
-        this.sort = sort;
-    }
+	public void setSort(Integer sort) {
+		this.sort = sort;
+	}
 
-    public Integer getCount() {
-        return count;
-    }
+	public Integer getCount() {
+		return count;
+	}
 
-    public void setCount(Integer count) {
-        this.count = count;
-    }
+	public void setCount(Integer count) {
+		this.count = count;
+	}
 
-    /**
-     * @return the parentId
-     */
-    public String getParentId() {
-        return parentId;
-    }
+	/**
+	 * @return the parentId
+	 */
+	public String getParentId() {
+		return parentId;
+	}
 
-    /**
-     * @param parentId the parentId to set
-     */
-    public void setParentId(String parentId) {
-        this.parentId = parentId;
-    }
+	/**
+	 * @param parentId
+	 *            the parentId to set
+	 */
+	public void setParentId(String parentId) {
+		this.parentId = parentId;
+	}
 
-    /**
-     * <pre>
-     * 功能描述：
-     * 使用示范：
-     *
-     * @return
-     * </pre>
-     */
-    @Override
-    public String toString() {
-        return "{id:" + id + ", orgName:" + orgName + ", orgTypeId:" + orgTypeId + ", orgTypeName:"
-               + orgTypeName + ", parentId=" + parentId + ", areaId:" + areaId + ", areaName:"
-               + areaName + ", countactPerson:" + countactPerson + ", countactPhone:"
-               + countactPhone + ", note:" + note + ", sort:" + sort + ", count:" + count + "}";
-    }
+	public Timestamp getGmtCreate() {
+		return gmtCreate;
+	}
+
+	public void setGmtCreate(Timestamp gmtCreate) {
+		this.gmtCreate = gmtCreate;
+	}
+
+	@Override
+	public String toString() {
+		return "OrgVO [id=" + id + ", orgName=" + orgName + ", orgTypeId=" + orgTypeId + ", orgTypeName=" + orgTypeName
+				+ ", parentId=" + parentId + ", areaId=" + areaId + ", areaName=" + areaName + ", countactPerson="
+				+ countactPerson + ", countactPhone=" + countactPhone + ", note=" + note + ", sort=" + sort + ", count="
+				+ count + ", gmtCreate=" + gmtCreate + "]";
+	}
 
 }
