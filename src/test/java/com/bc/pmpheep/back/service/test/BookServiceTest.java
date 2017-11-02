58ad453a149e5d121c6dac61920ffcc67763ca1a
zqf050217 <791038935@qq.com>
2017-11-02 16:27:47 +0800

    修改book.xml中的if判断

---
+++ src/test/java/com/bc/pmpheep/back/service/test/BookServiceTest.java
@@ -0,0 +1,11 @@
+package com.bc.pmpheep.back.service.test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.bc.pmpheep.test.BaseTest;
+
+public class BookServiceTest extends BaseTest {
+	Logger logger = LoggerFactory.getLogger(BookServiceTest.class);
+
+}
