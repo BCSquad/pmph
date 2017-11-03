2fc675a7a5a326f7c62d09c3cd524fc658074b9d
mao9593 <mao9593@users.noreply.github.com>
2017-11-03 17:11:22 +0800

    改进图五 数据迁移工具（已测试）

--- src/main/java/com/bc/pmpheep/migration/MigrationStageFive.java
+++ src/main/java/com/bc/pmpheep/migration/MigrationStageFive.java
@@ -18,7 +18,7 @@ import com.bc.pmpheep.migration.common.SQLParameters;
 import com.bc.pmpheep.utils.ExcelHelper;
 
 /**
- * 图五教材书籍迁移工具类
+ * 图五 	教材书籍迁移工具类
  * @author Mr
  *
  */
@@ -39,13 +39,13 @@ public class MigrationStageFive {
 	 * 教材书籍表
 	 */
 	private void textbook() {
-		String sql="select a.*,b.new_pk newmaterid,ifnull(c.new_pk,d.new_pk) bookcreateuserid,b.createdate newcreatedate,e.new_pk editroid "
-				+ " from teach_bookinfo a"
+		String sql="select a.*,b.new_pk newmaterid,c.new_pk bookcreateuserid,b.createdate newcreatedate,"
+				+ " e.new_pk editroid,d.new_pk newcreateuserid,b.createuserid oldcreateuserid"
+				+ " from teach_bookinfo a "
 				+ " left join teach_material b on b.materid=a.materid "
-				+ " left join sys_user c on c.userid=a.createuserid  "
-				+ " left join sys_user d on d.userid=b.createuserid  "
-				+ " left join sys_user e on e.userid=a.userid  "
-				;
+				+ " left join sys_user c on c.userid=a.createuserid "
+				+ " left join sys_user d on d.userid=b.createuserid "
+				+ " left join sys_user e on e.userid=a.userid  ";
 		String tableName="teach_bookinfo";// 要迁移的旧表名称
 		JdbcHelper.addColumn(tableName); // 增加new_pk字段
         List<Map<String, Object>> maps = JdbcHelper.getJdbcTemplate().queryForList(sql);
@@ -60,11 +60,8 @@ public class MigrationStageFive {
             Integer revision = (Integer) map.get("revision");
             if( null != revision){
             	textbook.setTextbookRound(revision); //书籍轮次
-            }else{
-            	map.put(SQLParameters.EXCEL_EX_HEADER, "书籍轮次为空");
-            	excel.add(map);
-            	logger.error("书籍轮次为空，此结果将被记录在Excel中");
-            	continue;
+            } else {
+            	textbook.setTextbookRound(1);//没有值，则书籍轮次默认为1
             }
             Long c = (Long) map.get("newmaterid");
             if (null != c) {
@@ -76,15 +73,20 @@ public class MigrationStageFive {
 				continue;
 			}
             Long createuserid = (Long) map.get("bookcreateuserid");
-            if (null != createuserid) {
-            	textbook.setFounderId(createuserid); // 创建者id 
+            Long newcreateuseid=(Long) map.get("newcreateuserid");
+            if (null != createuserid||null != newcreateuseid) {
+            	if(createuserid==newcreateuseid){// 如果没有创建者id 就找教材创建者id
+            		textbook.setFounderId(createuserid); // 创建者id
+            	}else{
+            		textbook.setFounderId(newcreateuseid); // 创建者id	
+            	}
 			} else {
-				map.put(SQLParameters.EXCEL_EX_HEADER, "创建者id为空");
+				/*记录教材书籍表没有的创建者id为空*/
+				map.put(SQLParameters.EXCEL_EX_HEADER, "创建者id为空，");
 				excel.add(map);
 				logger.error("创建者id为空，此结果将将被记录在Excel中");
 				continue;
 			}
-           
             String bookname = (String) map.get("bookname");
             if(StringUtil.isEmpty(bookname)){
             	map.put(SQLParameters.EXCEL_EX_HEADER, "书籍名称为空");
@@ -92,17 +94,25 @@ public class MigrationStageFive {
 				logger.error("书籍名称为空，此结果将将被记录在Excel中");
 				continue;
             }
-            java.util.Date createdate = (java.util.Date) map.get("newcreatedate");
-            if(null==createdate){
+            java.util.Date ceDate = (java.util.Date) map.get("createdate");
+            /*教材表对应书籍创建时间*/
+        	java.util.Date createdate = (java.util.Date) map.get("newcreatedate");
+            if(null != ceDate||null !=createdate){//如果没有创建时间 就查找关联教材创建时间
+            	if(ceDate==createdate){
+            		textbook.setGmtCreate((Timestamp) ceDate);// 创建时间
+            	}else{
+            		textbook.setGmtCreate((Timestamp) createdate);
+            	}
+            } else {
             	map.put(SQLParameters.EXCEL_EX_HEADER, "创建时间为空");
 				excel.add(map);
-				logger.error("创建时间为空，此结果将将被记录在Excel中");
+				logger.error("创建时间为空，因新库不能插入null，去教材表找对应创建时间，此结果将将被记录在Excel中");
 				continue;
-            }
+			}
             Integer xnumber = (Integer) map.get("xnumber");
             if(null != xnumber){
             	textbook.setSort(xnumber);// 图书序号
-            }else {
+            } else {
             	map.put(SQLParameters.EXCEL_EX_HEADER, "图书序号为空");
 				excel.add(map);
 				logger.error("图书序号为空，此结果将将被记录在Excel中");
@@ -111,7 +121,6 @@ public class MigrationStageFive {
             textbook.setPlanningEditor((Long) map.get("editroid"));// 策划编辑id
             textbook.setTextbookName(bookname);// 书籍名称
             textbook.setGmtPublished((Timestamp) map.get("resultpublishdate"));// 公布时间
-            textbook.setGmtCreate((Timestamp) (createdate));// 创建时间
             textbook.setIsbn((String) map.get("isbn"));// ISBN号
             textbook.setIsLocked(true);// 是否锁定（通过）旧平台无该状态 默认0
             textbook.setRevisionTimes(0);// 公布后再次修改次数  旧平台无该状态 默认0
@@ -122,27 +131,27 @@ public class MigrationStageFive {
              * 40：第一主编已选编委；50：策划编辑审核通过编委；60：项目编辑审核通过编委；70：主任审核通过；80：已发布
              */
             Integer dealtype=Integer.parseInt((String) map.get("dealtype"));
-            if(dealtype>=12){
+            if(dealtype >= 12){
             	textbook.setIsChiefChosen(true);// 是否已选定第一主编
             }
-            if(dealtype>=30){
+            if(dealtype >= 30){
             	textbook.setIsQualifierSelected(true);// 是否已预选编委
             }
-            if(dealtype>=40){
+            if(dealtype >= 40){
             	textbook.setIsListSelected(true);// 主编是否选定编委
             }
-            if(dealtype>=50){
+            if(dealtype >= 50){
             	textbook.setIsPlanningEditorConfirm(true);// 策划编辑是否确定名单
             }
-            if(dealtype>=60){
+            if(dealtype >= 60){
             	textbook.setIsProjectEditorConfirm(true);// 项目编辑是否确定名单
             }
-            if(dealtype>=80){
+            if(dealtype >= 80){
             	textbook.setIsPublished(true);// 是否已公布
             }
             /* 开始新增新表对象，并设置属性值 */
             textbook = textbookService.addTextbook(textbook);
-        	long pk =textbook.getId();
+        	long pk = textbook.getId();
         	JdbcHelper.updateNewPrimaryKey(tableName, pk, "bookid", id); // 更新旧表中new_pk字段
         	count++;
         }
