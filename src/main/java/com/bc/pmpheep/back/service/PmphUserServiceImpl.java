0c3c6eb379cf83afb63797b399b9486cfcfd0312
Administrator <Administrator@USERCHI-FLMBVIN.lan>
2017-11-06 21:09:28 +0800

    修改添加社内用户的验证

--- src/main/java/com/bc/pmpheep/back/service/PmphUserServiceImpl.java
+++ src/main/java/com/bc/pmpheep/back/service/PmphUserServiceImpl.java
@@ -85,10 +85,10 @@ public class PmphUserServiceImpl implements PmphUserService {
 	@Override
 	public PmphUser add(PmphUser user, List<Long> rids) throws CheckedServiceException {
 		Long userId = this.add(user).getId();
-		if (!ObjectUtil.isNull(userId)) {
-			throw new CheckedServiceException(CheckedExceptionBusiness.USER_MANAGEMENT,
-					CheckedExceptionResult.NULL_PARAM, "用户ID为空时不能添加角色！");
-		}
+		// if (!ObjectUtil.isNull(userId)) {
+		// throw new CheckedServiceException(CheckedExceptionBusiness.USER_MANAGEMENT,
+		// CheckedExceptionResult.NULL_PARAM, "用户ID为空时不能添加角色！");
+		// }
 		roleDao.addUserRoles(userId, rids);
 		return user;
 	}
