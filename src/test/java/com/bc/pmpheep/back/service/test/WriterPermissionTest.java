d7a61c11fce63efbdb424d2baa030419b64c4c81
zqf050217 <791038935@qq.com>
2017-11-07 17:36:16 +0800

    补充单元测试

--- src/test/java/com/bc/pmpheep/back/service/test/WriterPermissionTest.java
+++ src/test/java/com/bc/pmpheep/back/service/test/WriterPermissionTest.java
@@ -17,68 +17,70 @@ import com.bc.pmpheep.test.BaseTest;
  * 
  */
 public class WriterPermissionTest extends BaseTest {
-    @SuppressWarnings("unused")
-    private static final Logger logger = LoggerFactory.getLogger(WriterPermissionTest.class);
+	@SuppressWarnings("unused")
+	private static final Logger logger = LoggerFactory.getLogger(WriterPermissionTest.class);
 
-    @Autowired
-    WriterPermissionService     writerPermissionService;
+	@Autowired
+	WriterPermissionService writerPermissionService;
 
-    /**
-     * PmphPermission 添加Test
-     */
-    @Test
-    // @Rollback(Const.ISROLLBACK)
-    public void addPmphPermissionTest() {
-        WriterPermission pp = new WriterPermission();
-        pp.setMenuName("用户管理");
-        pp.setUrl("admin/user/add");
-        pp.setPeermissionName("用户管理添加");
-        pp.setPath("admin:add");
-        pp.setParentId(1L);
-        Integer aInteger = writerPermissionService.addWriterPermission(pp);// 添加资源目录
-        Assert.assertTrue("是否添加成功", aInteger > 0 ? true : false);
+	/**
+	 * PmphPermission 添加Test
+	 */
+	@Test
+	// @Rollback(Const.ISROLLBACK)
+	public void addPmphPermissionTest() {
+		WriterPermission pp = new WriterPermission();
+		pp.setMenuName("用户管理");
+		pp.setUrl("admin/user/add");
+		pp.setPermissionName("用户管理添加");
+		pp.setPath("admin:add");
+		pp.setParentId(1L);
+		Integer aInteger = writerPermissionService.addWriterPermission(pp);// 添加资源目录
+		Assert.assertTrue("是否添加成功", aInteger > 0 ? true : false);
+	}
 
-    }
+	/**
+	 * PmphPermission 更新Test
+	 */
+	@Test
+	// @Rollback(Const.ISROLLBACK)
+	public void updatePmphPermissionTest() {
+		WriterPermission pp = new WriterPermission();
+		pp.setId(1L);
+		pp.setMenuName("用户管理1");
+		Integer aInteger = writerPermissionService.updateWriterPermissionById(pp);// 按ID更新资源目录
+		Assert.assertTrue("是否更新成功", aInteger > 0 ? true : false);
+	}
 
-    /**
-     * PmphPermission 更新Test
-     */
-    // @Test
-    // @Rollback(Const.ISROLLBACK)
-    public void updatePmphPermissionTest() {
-        WriterPermission pp = new WriterPermission();
-        pp.setId(1L);
-        pp.setMenuName("用户管理1");
-        Integer aInteger = writerPermissionService.updateWriterPermissionById(pp);// 按ID更新资源目录
-        Assert.assertTrue("是否更新成功", aInteger > 0 ? true : false);
-    }
+	/**
+	 * PmphPermission 删除Test
+	 */
+	@Test
+	// @Rollback(Const.ISROLLBACK)
+	public void deletePmphPermissionTest() {
+		WriterPermission pp = new WriterPermission();
+		pp.setId(1L);
+		String[] ids = { "1L" };
+		Integer aInteger = writerPermissionService.delete(1L);// 按ID删除资源
+		Assert.assertTrue("delete是否删除成功", aInteger > 0 ? true : false);
+		Integer bInteger = writerPermissionService.deleteWriterPermissionById(ids);
+		Assert.assertTrue("deleteWriterPermissionById是否删除成功", bInteger > 0 ? true : false);
+	}
 
-    /**
-     * PmphPermission 删除Test
-     */
-    // @Test
-    // @Rollback(Const.ISROLLBACK)
-    public void deletePmphPermissionTest() {
-        WriterPermission pp = new WriterPermission();
-        pp.setId(1L);
-        String[] ids = { "1L" };
-        Integer aInteger = writerPermissionService.delete(1L);// 按ID删除资源
-        Assert.assertTrue("delete是否删除成功", aInteger > 0 ? true : false);
-        Integer bInteger = writerPermissionService.deleteWriterPermissionById(ids);
-        Assert.assertTrue("deleteWriterPermissionById是否删除成功", bInteger > 0 ? true : false);
-    }
+	/**
+	 * PmphPermission 查询Test
+	 */
+	@Test
+	// @Rollback(Const.ISROLLBACK)
+	public void getPmphPermissionTest() {
+		WriterPermission pp = new WriterPermission();
+		pp.setId(1L);
+		Assert.assertNotNull("getWriterPermissionByPermissionName是否为空",
+				writerPermissionService.getWriterPermissionByPermissionName("用户管理添加"));// 按ID查询资源
+		Assert.assertNotNull("get是否为空", writerPermissionService.get(1L));
+		Assert.assertNotNull("getListResource是否为空", writerPermissionService.getListResource());// 查询所有资源
+		Assert.assertNotNull("WriterPermission是否为空", writerPermissionService.getListAllParentMenu());
+		Assert.assertNotNull("WriterPermissionByParentId是否为空", writerPermissionService.getListChildMenuByParentId(0L));
 
-    /**
-     * PmphPermission 查询Test
-     */
-    // @Test
-    // @Rollback(Const.ISROLLBACK)
-    public void getPmphPermissionTest() {
-        WriterPermission pp = new WriterPermission();
-        pp.setId(1L);
-        Assert.assertNotNull("getWriterPermissionByPermissionName是否为空",
-                             writerPermissionService.getWriterPermissionByPermissionName("用户管理添加"));// 按ID查询资源
-        Assert.assertNotNull("get是否为空", writerPermissionService.get(1L));
-        Assert.assertNotNull("getListResource是否为空", writerPermissionService.getListResource());// 查询所有资源
-    }
+	}
 }
