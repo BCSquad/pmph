9a62d0f9e32ae0ae2a6d508586bb4e16f21140bb
zqf050217 <791038935@qq.com>
2017-11-09 15:06:10 +0800

    添加用户真实姓名为空时的逻辑

--- src/main/java/com/bc/pmpheep/back/service/OrgUserServiceImpl.java
+++ src/main/java/com/bc/pmpheep/back/service/OrgUserServiceImpl.java
@@ -70,7 +70,7 @@ public class OrgUserServiceImpl extends BaseService implements OrgUserService {
 					CheckedExceptionResult.ILLEGAL_PARAM, "用户名不能超过20个字符");
 		}
 
-		if (null == orgUser.getRealname()) {
+		if (StringUtil.isEmpty(orgUser.getRealname())) {
 			orgUser.setRealname(orgUser.getUsername());
 		}
 		orgUserDao.addOrgUser(orgUser);
