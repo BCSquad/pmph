59ae54b1b976b1b11bb58480b6565ba11cbd069c
lyc <513760648@qq.com>
2017-11-10 14:53:37 +0800

    增加一个全局变量number用于判断社内用户中关联学校的错误数据  by lyc

--- src/main/java/com/bc/pmpheep/migration/MigrationStageTwo.java
+++ src/main/java/com/bc/pmpheep/migration/MigrationStageTwo.java
@@ -41,6 +41,7 @@ import com.bc.pmpheep.utils.ExcelHelper;
 public class MigrationStageTwo {
 
 	private final Logger logger = LoggerFactory.getLogger(MigrationStageTwo.class);
+	private int number = 0 ;
 	
 	@Resource
 	ExcelHelper excelHelper;
@@ -64,7 +65,7 @@ public class MigrationStageTwo {
 		cannotFindRole();
 	}
 	
-	protected int pmphDepartment() {
+	protected void pmphDepartment() {
 		String tableName = "ba_organize";//图一此表已添加new_pk
 		String sql = "SELECT orgid,parentid,orgname,sortno,remark "
 				   + "FROM ba_organize WHERE orgcode "
@@ -100,7 +101,8 @@ public class MigrationStageTwo {
             pmphDepartment = pmphDepartmentService.add(pmphDepartment);
             Long pk = pmphDepartment.getId();
             JdbcHelper.updateNewPrimaryKey(tableName, pk, "orgid", departmentId);            
-			count++;			
+			count++;
+			number++;
 		}
 		if(excel.size() > 0){
 			try {
@@ -115,7 +117,6 @@ public class MigrationStageTwo {
         Map<String,Object> msg= new HashMap<String,Object>();
         msg.put("result", "pmph_department表迁移完成"+count+"/"+ maps.size());
         SQLParameters.msg.add(msg);
-        return count; 
 	}
 	
 	protected void pmphUser() {
@@ -151,7 +152,7 @@ public class MigrationStageTwo {
 				  realName = userName;
 			  }
 			  Long departmentId = (Long) map.get("new_pk");
-			  if (ObjectUtil.isNull(departmentId) || departmentId > pmphDepartment()){
+			  if (ObjectUtil.isNull(departmentId) || departmentId > number){
 				  departmentId = 0L;
 				  map.put(SQLParameters.EXCEL_EX_HEADER, sb.append("找不到对应社内部门或连接的是学校机构。"));
 				  excel.add(map);
