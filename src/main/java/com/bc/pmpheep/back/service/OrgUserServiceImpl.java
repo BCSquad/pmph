03af35d66b2aad8972cf67e889aa52750f7c000b
nyz526 <nyz526@163.com>
2017-11-14 18:27:05 +0800

    学校教师审核，代码优化

--- src/main/java/com/bc/pmpheep/back/service/OrgUserServiceImpl.java
+++ src/main/java/com/bc/pmpheep/back/service/OrgUserServiceImpl.java
@@ -12,8 +12,7 @@ import com.bc.pmpheep.back.plugin.PageParameter;
 import com.bc.pmpheep.back.plugin.PageResult;
 import com.bc.pmpheep.back.po.OrgUser;
 import com.bc.pmpheep.back.shiro.kit.ShiroKit;
-import com.bc.pmpheep.back.util.ArrayUtil;
-import com.bc.pmpheep.back.util.CastUtil;
+import com.bc.pmpheep.back.util.CollectionUtil;
 import com.bc.pmpheep.back.util.Const;
 import com.bc.pmpheep.back.util.ObjectUtil;
 import com.bc.pmpheep.back.util.PageParameterUitl;
@@ -32,216 +31,215 @@ import com.bc.pmpheep.service.exception.CheckedServiceException;
  */
 @Service
 public class OrgUserServiceImpl extends BaseService implements OrgUserService {
-	@Autowired
-	private OrgUserDao orgUserDao;
+    @Autowired
+    private OrgUserDao orgUserDao;
 
-	@Override
-	public List<OrgUser> getOrgUserListByOrgIds(List<Long> orgIds) throws CheckedServiceException {
-		if (null == orgIds) {
-			throw new CheckedServiceException(CheckedExceptionBusiness.USER_MANAGEMENT,
-					CheckedExceptionResult.NULL_PARAM, "参数为空");
-		}
-		return orgUserDao.getOrgUserListByOrgIds(orgIds);
-	}
+    @Override
+    public List<OrgUser> getOrgUserListByOrgIds(List<Long> orgIds) throws CheckedServiceException {
+        if (null == orgIds) {
+            throw new CheckedServiceException(CheckedExceptionBusiness.USER_MANAGEMENT,
+                                              CheckedExceptionResult.NULL_PARAM, "参数为空");
+        }
+        return orgUserDao.getOrgUserListByOrgIds(orgIds);
+    }
 
-	/**
-	 * 
-	 * @param orgUser
-	 *            实体对象
-	 * @return 带主键的 OrgUser
-	 * @throws CheckedServiceException
-	 */
-	@Override
-	public OrgUser addOrgUser(OrgUser orgUser) throws CheckedServiceException {
-		if (ObjectUtil.isNull(orgUser)) {
-			throw new CheckedServiceException(CheckedExceptionBusiness.USER_MANAGEMENT,
-					CheckedExceptionResult.ILLEGAL_PARAM, "参数为空");
-		}
-		if (StringUtil.isEmpty(orgUser.getUsername())) {
-			throw new CheckedServiceException(CheckedExceptionBusiness.USER_MANAGEMENT,
-					CheckedExceptionResult.ILLEGAL_PARAM, "用户名不能为空");
-		}
-		if (StringUtil.isEmpty(orgUser.getPassword())) {
-			throw new CheckedServiceException(CheckedExceptionBusiness.USER_MANAGEMENT,
-					CheckedExceptionResult.ILLEGAL_PARAM, "密码不能为空");
-		}
-		if (StringUtil.strLength(orgUser.getUsername()) > 20) {
-			throw new CheckedServiceException(CheckedExceptionBusiness.USER_MANAGEMENT,
-					CheckedExceptionResult.ILLEGAL_PARAM, "用户名不能超过20个字符");
-		}
+    /**
+     * 
+     * @param orgUser 实体对象
+     * @return 带主键的 OrgUser
+     * @throws CheckedServiceException
+     */
+    @Override
+    public OrgUser addOrgUser(OrgUser orgUser) throws CheckedServiceException {
+        if (ObjectUtil.isNull(orgUser)) {
+            throw new CheckedServiceException(CheckedExceptionBusiness.USER_MANAGEMENT,
+                                              CheckedExceptionResult.ILLEGAL_PARAM, "参数为空");
+        }
+        if (StringUtil.isEmpty(orgUser.getUsername())) {
+            throw new CheckedServiceException(CheckedExceptionBusiness.USER_MANAGEMENT,
+                                              CheckedExceptionResult.ILLEGAL_PARAM, "用户名不能为空");
+        }
+        if (StringUtil.isEmpty(orgUser.getPassword())) {
+            throw new CheckedServiceException(CheckedExceptionBusiness.USER_MANAGEMENT,
+                                              CheckedExceptionResult.ILLEGAL_PARAM, "密码不能为空");
+        }
+        if (StringUtil.strLength(orgUser.getUsername()) > 20) {
+            throw new CheckedServiceException(CheckedExceptionBusiness.USER_MANAGEMENT,
+                                              CheckedExceptionResult.ILLEGAL_PARAM, "用户名不能超过20个字符");
+        }
 
-		if (StringUtil.isEmpty(orgUser.getRealname())) {
-			orgUser.setRealname(orgUser.getUsername());
-		}
-		orgUserDao.addOrgUser(orgUser);
-		return orgUser;
-	}
+        if (StringUtil.isEmpty(orgUser.getRealname())) {
+            orgUser.setRealname(orgUser.getUsername());
+        }
+        orgUserDao.addOrgUser(orgUser);
+        return orgUser;
+    }
 
-	/**
-	 * 
-	 * @param id
-	 * @return OrgUser
-	 * @throws CheckedServiceException
-	 */
-	@Override
-	public OrgUser getOrgUserById(Long id) throws CheckedServiceException {
-		if (null == id) {
-			throw new CheckedServiceException(CheckedExceptionBusiness.USER_MANAGEMENT,
-					CheckedExceptionResult.NULL_PARAM, "主键为空");
-		}
-		return orgUserDao.getOrgUserById(id);
-	}
+    /**
+     * 
+     * @param id
+     * @return OrgUser
+     * @throws CheckedServiceException
+     */
+    @Override
+    public OrgUser getOrgUserById(Long id) throws CheckedServiceException {
+        if (null == id) {
+            throw new CheckedServiceException(CheckedExceptionBusiness.USER_MANAGEMENT,
+                                              CheckedExceptionResult.NULL_PARAM, "主键为空");
+        }
+        return orgUserDao.getOrgUserById(id);
+    }
 
-	/**
-	 * 
-	 * @param id
-	 * @return 影响行数
-	 * @throws CheckedServiceException
-	 */
-	@Override
-	public Integer deleteOrgUserById(Long id) throws CheckedServiceException {
-		if (null == id) {
-			throw new CheckedServiceException(CheckedExceptionBusiness.USER_MANAGEMENT,
-					CheckedExceptionResult.NULL_PARAM, "主键为空");
-		}
-		return orgUserDao.deleteOrgUserById(id);
-	}
+    /**
+     * 
+     * @param id
+     * @return 影响行数
+     * @throws CheckedServiceException
+     */
+    @Override
+    public Integer deleteOrgUserById(Long id) throws CheckedServiceException {
+        if (null == id) {
+            throw new CheckedServiceException(CheckedExceptionBusiness.USER_MANAGEMENT,
+                                              CheckedExceptionResult.NULL_PARAM, "主键为空");
+        }
+        return orgUserDao.deleteOrgUserById(id);
+    }
 
-	/**
-	 * @param orgUser
-	 * @return 影响行数
-	 * @throws CheckedServiceException
-	 */
-	@Override
-	public Integer updateOrgUser(OrgUser orgUser) throws CheckedServiceException {
-		if (null == orgUser.getId()) {
-			throw new CheckedServiceException(CheckedExceptionBusiness.USER_MANAGEMENT,
-					CheckedExceptionResult.NULL_PARAM, "主键为空");
-		}
-		return orgUserDao.updateOrgUser(orgUser);
-	}
+    /**
+     * @param orgUser
+     * @return 影响行数
+     * @throws CheckedServiceException
+     */
+    @Override
+    public Integer updateOrgUser(OrgUser orgUser) throws CheckedServiceException {
+        if (null == orgUser.getId()) {
+            throw new CheckedServiceException(CheckedExceptionBusiness.USER_MANAGEMENT,
+                                              CheckedExceptionResult.NULL_PARAM, "主键为空");
+        }
+        return orgUserDao.updateOrgUser(orgUser);
+    }
 
-	@Override
-	public Integer updateOrgUserProgressById(Integer progress, String[] orgUserIds) throws CheckedServiceException {
-		if (ArrayUtil.isEmpty(orgUserIds) || ObjectUtil.isNull(progress)) {
-			throw new CheckedServiceException(CheckedExceptionBusiness.SCHOOL_ADMIN_CHECK,
-					CheckedExceptionResult.NULL_PARAM, "参数为空");
-		}
-		List<OrgUser> orgUserList = new ArrayList<OrgUser>(orgUserIds.length);
-		for (String orgId : orgUserIds) {
-			OrgUser orgUser = orgUserDao.getOrgUserById(CastUtil.castLong(orgId));
-			if (orgUser.getProgress() != 0) {
-				throw new CheckedServiceException(CheckedExceptionBusiness.SCHOOL_ADMIN_CHECK,
-						CheckedExceptionResult.NULL_PARAM, "用户已经审核过了");
-			}
-			orgUserList.add(new OrgUser(CastUtil.castLong(orgId), progress));
-		}
-		return orgUserDao.updateOrgUserProgressById(orgUserList);
-	}
+    @Override
+    public Integer updateOrgUserProgressById(Integer progress, List<Long> orgUserIds)
+    throws CheckedServiceException {
+        if (CollectionUtil.isEmpty(orgUserIds) || ObjectUtil.isNull(progress)) {
+            throw new CheckedServiceException(CheckedExceptionBusiness.SCHOOL_ADMIN_CHECK,
+                                              CheckedExceptionResult.NULL_PARAM, "参数为空");
+        }
+        List<OrgUser> orgUserList = orgUserDao.getOrgUserByIds(orgUserIds);
+        List<OrgUser> orgUsers = new ArrayList<OrgUser>(orgUserList.size());
+        for (OrgUser orgUser : orgUserList) {
+            if (Const.ORG_USER_PROGRESS_0 == orgUser.getProgress()) {
+                orgUsers.add(new OrgUser(orgUser.getId(), progress));
+            }
+        }
+        return orgUserDao.updateOrgUserProgressById(orgUsers);
+    }
 
-	/**
-	 * 
-	 * 功能描述：分页查询作家用户
-	 * 
-	 * @param page
-	 *            传入的查询数据
-	 * @return 需要的Page对象
-	 */
-	@Override
-	public PageResult<OrgUserManagerVO> getListOrgUser(PageParameter<OrgUserManagerVO> pageParameter)
-			throws CheckedServiceException {
-		String username = pageParameter.getParameter().getUsername();
-		if (StringUtil.notEmpty(username)) {
-			pageParameter.getParameter().setUsername(username);
-		}
-		String realname = pageParameter.getParameter().getRealname();
-		if (StringUtil.notEmpty(realname)) {
-			pageParameter.getParameter().setRealname(realname);
-		}
-		String orgName = pageParameter.getParameter().getOrgName();
-		if (StringUtil.notEmpty(orgName)) {
-			pageParameter.getParameter().setOrgName(orgName);
-		}
-		PageResult<OrgUserManagerVO> pageResult = new PageResult<OrgUserManagerVO>();
-		PageParameterUitl.CopyPageParameter(pageParameter, pageResult);
-		int total = orgUserDao.getListOrgUserTotal(pageParameter);
-		if (total > 0) {
-			pageResult.setTotal(total);
-			List<OrgUserManagerVO> list = orgUserDao.getListOrgUser(pageParameter);
-			pageResult.setRows(list);
-		}
-		return pageResult;
-	}
+    /**
+     * 
+     * 功能描述：分页查询作家用户
+     * 
+     * @param page 传入的查询数据
+     * @return 需要的Page对象
+     */
+    @Override
+    public PageResult<OrgUserManagerVO> getListOrgUser(PageParameter<OrgUserManagerVO> pageParameter)
+    throws CheckedServiceException {
+        String username = pageParameter.getParameter().getUsername();
+        if (StringUtil.notEmpty(username)) {
+            pageParameter.getParameter().setUsername(username);
+        }
+        String realname = pageParameter.getParameter().getRealname();
+        if (StringUtil.notEmpty(realname)) {
+            pageParameter.getParameter().setRealname(realname);
+        }
+        String orgName = pageParameter.getParameter().getOrgName();
+        if (StringUtil.notEmpty(orgName)) {
+            pageParameter.getParameter().setOrgName(orgName);
+        }
+        PageResult<OrgUserManagerVO> pageResult = new PageResult<OrgUserManagerVO>();
+        PageParameterUitl.CopyPageParameter(pageParameter, pageResult);
+        int total = orgUserDao.getListOrgUserTotal(pageParameter);
+        if (total > 0) {
+            pageResult.setTotal(total);
+            List<OrgUserManagerVO> list = orgUserDao.getListOrgUser(pageParameter);
+            pageResult.setRows(list);
+        }
+        return pageResult;
+    }
 
-	@Override
-	public String addOrgUserOfBack(OrgUser orgUser) throws CheckedServiceException {
-		if (StringUtil.isEmpty(orgUser.getUsername())) {
-			throw new CheckedServiceException(CheckedExceptionBusiness.USER_MANAGEMENT,
-					CheckedExceptionResult.NULL_PARAM, "机构代码不能为空");
-		}
-		if (StringUtil.strLength(orgUser.getUsername()) > 20) {
-			throw new CheckedServiceException(CheckedExceptionBusiness.USER_MANAGEMENT,
-					CheckedExceptionResult.ILLEGAL_PARAM, "用户名不能超过20个字符");
-		}
-		if (!StringUtil.isEmpty(orgUser.getNote())) {
-			if (StringUtil.strLength(orgUser.getNote()) > 100) {
-				throw new CheckedServiceException(CheckedExceptionBusiness.USER_MANAGEMENT,
-						CheckedExceptionResult.ILLEGAL_PARAM, "备注不能超过100个字符");
-			}
-		}
-		if (null == orgUser.getRealname()) {
-			orgUser.setRealname(orgUser.getUsername());
-		}
-		orgUser.setPassword(ShiroKit.md5(Const.DEFAULT_PASSWORD, orgUser.getUsername()));// 后台添加用户设置默认密码为123456
-		int num = orgUserDao.addOrgUser(orgUser);// 返回的影响行数，如果不是影响0行就是添加成功
-		String result = "FAIL";
-		if (num > 0) {
-			result = "SUCCESS";
-		}
-		return result;
-	}
+    @Override
+    public String addOrgUserOfBack(OrgUser orgUser) throws CheckedServiceException {
+        if (StringUtil.isEmpty(orgUser.getUsername())) {
+            throw new CheckedServiceException(CheckedExceptionBusiness.USER_MANAGEMENT,
+                                              CheckedExceptionResult.NULL_PARAM, "机构代码不能为空");
+        }
+        if (StringUtil.strLength(orgUser.getUsername()) > 20) {
+            throw new CheckedServiceException(CheckedExceptionBusiness.USER_MANAGEMENT,
+                                              CheckedExceptionResult.ILLEGAL_PARAM, "用户名不能超过20个字符");
+        }
+        if (!StringUtil.isEmpty(orgUser.getNote())) {
+            if (StringUtil.strLength(orgUser.getNote()) > 100) {
+                throw new CheckedServiceException(CheckedExceptionBusiness.USER_MANAGEMENT,
+                                                  CheckedExceptionResult.ILLEGAL_PARAM,
+                                                  "备注不能超过100个字符");
+            }
+        }
+        if (null == orgUser.getRealname()) {
+            orgUser.setRealname(orgUser.getUsername());
+        }
+        orgUser.setPassword(ShiroKit.md5(Const.DEFAULT_PASSWORD, orgUser.getUsername()));// 后台添加用户设置默认密码为123456
+        int num = orgUserDao.addOrgUser(orgUser);// 返回的影响行数，如果不是影响0行就是添加成功
+        String result = "FAIL";
+        if (num > 0) {
+            result = "SUCCESS";
+        }
+        return result;
+    }
 
-	@Override
-	public String updateOrgUserOfBack(OrgUser orgUser) throws CheckedServiceException {
-		OrgUser username=orgUserDao.getOrgUserById(orgUser.getId());
-		if(!username.getUsername().equals(orgUser.getUsername())){
-			throw new CheckedServiceException(CheckedExceptionBusiness.USER_MANAGEMENT,
-					CheckedExceptionResult.ILLEGAL_PARAM, "机构代码不相同");
-		}
-		if (ObjectUtil.isNull(orgUser.getId())) {
-			throw new CheckedServiceException(CheckedExceptionBusiness.USER_MANAGEMENT,
-					CheckedExceptionResult.NULL_PARAM, "主键为空");
-		}
-		if (StringUtil.strLength(orgUser.getUsername()) > 20) {
-			throw new CheckedServiceException(CheckedExceptionBusiness.USER_MANAGEMENT,
-					CheckedExceptionResult.ILLEGAL_PARAM, "用户名不能超过20个字符");
-		}
-		if (StringUtil.isEmpty(orgUser.getUsername())) {
-			throw new CheckedServiceException(CheckedExceptionBusiness.USER_MANAGEMENT,
-					CheckedExceptionResult.NULL_PARAM, "机构代码不能为空");
-		}
-		if (StringUtil.strLength(orgUser.getRealname()) > 20) {
-			throw new CheckedServiceException(CheckedExceptionBusiness.USER_MANAGEMENT,
-					CheckedExceptionResult.ILLEGAL_PARAM, "管理员姓名不能超过20个字符");
-		}
-		if (StringUtil.strLength(orgUser.getNote()) > 100) {
-			throw new CheckedServiceException(CheckedExceptionBusiness.USER_MANAGEMENT,
-					CheckedExceptionResult.ILLEGAL_PARAM, "备注不能超过100个字符");
-		}
-		if (!ValidatUtil.checkEmail(orgUser.getEmail())) {
-			throw new CheckedServiceException(CheckedExceptionBusiness.USER_MANAGEMENT,
-					CheckedExceptionResult.ILLEGAL_PARAM, "邮箱不符合规范");
-		}
-		if (!ValidatUtil.checkMobileNumber(orgUser.getHandphone())) {
-			throw new CheckedServiceException(CheckedExceptionBusiness.USER_MANAGEMENT,
-					CheckedExceptionResult.ILLEGAL_PARAM, "手机号码不符合规范");
-		}
-		int num = orgUserDao.updateOrgUser(orgUser);// 返回的影响行数，如果不是影响0行就是添加成功
-		String result = "FAIL";
-		if (num > 0) {
-			result = "SUCCESS";
-		}
-		return result;
-	}
+    @Override
+    public String updateOrgUserOfBack(OrgUser orgUser) throws CheckedServiceException {
+        OrgUser username = orgUserDao.getOrgUserById(orgUser.getId());
+        if (!username.getUsername().equals(orgUser.getUsername())) {
+            throw new CheckedServiceException(CheckedExceptionBusiness.USER_MANAGEMENT,
+                                              CheckedExceptionResult.ILLEGAL_PARAM, "机构代码不相同");
+        }
+        if (ObjectUtil.isNull(orgUser.getId())) {
+            throw new CheckedServiceException(CheckedExceptionBusiness.USER_MANAGEMENT,
+                                              CheckedExceptionResult.NULL_PARAM, "主键为空");
+        }
+        if (StringUtil.strLength(orgUser.getUsername()) > 20) {
+            throw new CheckedServiceException(CheckedExceptionBusiness.USER_MANAGEMENT,
+                                              CheckedExceptionResult.ILLEGAL_PARAM, "用户名不能超过20个字符");
+        }
+        if (StringUtil.isEmpty(orgUser.getUsername())) {
+            throw new CheckedServiceException(CheckedExceptionBusiness.USER_MANAGEMENT,
+                                              CheckedExceptionResult.NULL_PARAM, "机构代码不能为空");
+        }
+        if (StringUtil.strLength(orgUser.getRealname()) > 20) {
+            throw new CheckedServiceException(CheckedExceptionBusiness.USER_MANAGEMENT,
+                                              CheckedExceptionResult.ILLEGAL_PARAM,
+                                              "管理员姓名不能超过20个字符");
+        }
+        if (StringUtil.strLength(orgUser.getNote()) > 100) {
+            throw new CheckedServiceException(CheckedExceptionBusiness.USER_MANAGEMENT,
+                                              CheckedExceptionResult.ILLEGAL_PARAM, "备注不能超过100个字符");
+        }
+        if (!ValidatUtil.checkEmail(orgUser.getEmail())) {
+            throw new CheckedServiceException(CheckedExceptionBusiness.USER_MANAGEMENT,
+                                              CheckedExceptionResult.ILLEGAL_PARAM, "邮箱不符合规范");
+        }
+        if (!ValidatUtil.checkMobileNumber(orgUser.getHandphone())) {
+            throw new CheckedServiceException(CheckedExceptionBusiness.USER_MANAGEMENT,
+                                              CheckedExceptionResult.ILLEGAL_PARAM, "手机号码不符合规范");
+        }
+        int num = orgUserDao.updateOrgUser(orgUser);// 返回的影响行数，如果不是影响0行就是添加成功
+        String result = "FAIL";
+        if (num > 0) {
+            result = "SUCCESS";
+        }
+        return result;
+    }
 
 }
