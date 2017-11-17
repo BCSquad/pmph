cb2aea10f4d33e645ac2d90da61172e603d77bd7
lyc <513760648@qq.com>
2017-11-17 19:09:50 +0800

    测试类修改，精确到方法一级，已跑通  by lyc

--- src/test/java/com/bc/pmpheep/back/service/test/DecLastPositionServiceTest.java
+++ src/test/java/com/bc/pmpheep/back/service/test/DecLastPositionServiceTest.java
@@ -3,15 +3,20 @@
  */
 package com.bc.pmpheep.back.service.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.test.annotation.Rollback;
 
 import com.bc.pmpheep.back.po.DecLastPosition;
 import com.bc.pmpheep.back.service.DecLastPositionService;
+import com.bc.pmpheep.back.util.Const;
 import com.bc.pmpheep.test.BaseTest;
 
 /**
@@ -28,20 +33,65 @@ public class DecLastPositionServiceTest extends BaseTest {
 	DecLastPositionService decLastPositionService;
 
 	@Test
-	public void test() {
-          logger.info("----------作家上套教材参编情况----------");
-          DecLastPosition decLastPosition = new DecLastPosition(7L, "心理诊断学", (Integer)1, "参编", 5);
-          decLastPositionService.addDecLastPosition(decLastPosition);
-          Assert.assertTrue("添加数据失败", decLastPosition.getId()>0);
-          logger.info("---------添加数据成功----------");
-          decLastPosition.setMaterialName("心理咨询技术");
-          Assert.assertTrue("数据更新失败", decLastPositionService.updateDecLastPosition(decLastPosition)>0);
-          logger.info("-------数据更新成功----------");
-          Assert.assertNotNull("获取数据失败", decLastPositionService.getDecLastPositionById(3L));
-          logger.info("-------单条信息获取成功---------");
-          Assert.assertTrue("数据集合获取失败", decLastPositionService.getListDecLastPositionByDeclarationId(7L).size()>0);
-          logger.info("-------数据集合获取成功-------");
-          Assert.assertTrue("数据删除失败", decLastPositionService.deleteDecLastPositionById(3L) >= 0);
-          logger.info("--------测试成功----------");
+	@Rollback(Const.ISROLLBACK)
+	public void testAddDecLastPosition(){
+		DecLastPosition decLastPosition = new DecLastPosition();
+		decLastPosition.setDeclarationId(23L);
+		decLastPosition.setMaterialName("管理心理学");
+		decLastPosition.setPosition(1);
+		decLastPosition.setNote("国家级教授");
+		decLastPosition.setSort(37);
+		decLastPosition = decLastPositionService.addDecLastPosition(decLastPosition);
+		Assert.assertTrue("添加数据失败", decLastPosition.getId() > 0);
+	}
+	
+	@Test
+	@Rollback(Const.ISROLLBACK)
+	public void testDeleteDecLastPosition(){
+		long id = add().getId();
+		Integer count = decLastPositionService.deleteDecLastPositionById(id);
+		Assert.assertTrue("删除数据失败", count > 0);
+	}
+	
+	@Test
+	@Rollback(Const.ISROLLBACK)
+	public void testUpdateDecLastPosition(){
+		DecLastPosition decLastPosition = add();
+		decLastPosition.setDeclarationId(3L);
+		decLastPosition.setMaterialName("心理统计学");
+		Integer count = decLastPositionService.updateDecLastPosition(decLastPosition);
+		Assert.assertTrue("数据更新失败", count > 0);
+	}
+	
+	@Test
+	@Rollback(Const.ISROLLBACK)
+	public void testGetDecLastPosition(){
+		long id = add().getId();
+		DecLastPosition decLastPosition = decLastPositionService.getDecLastPositionById(id);
+		Assert.assertNotNull("获取作家上套教材参编情况失败", decLastPosition);
+	}
+	
+	@Test
+	@Rollback(Const.ISROLLBACK)
+	public void testListDecLastPosition(){
+		add();
+		List<DecLastPosition> list = new ArrayList<>();
+		list = decLastPositionService.getListDecLastPositionByDeclarationId(5L);
+		Assert.assertTrue("获取作家上套教材参编情况集合失败", list.size() > 0);
+	}
+	
+	private DecLastPosition add(){
+		DecLastPosition decLastPosition = new DecLastPosition();
+		decLastPosition.setDeclarationId(5L);
+		decLastPosition.setMaterialName("心理诊断学");
+		decLastPosition.setPosition(2);
+		decLastPosition.setNote("研究人员");
+		decLastPosition.setSort(25);
+		decLastPositionService.addDecLastPosition(decLastPosition);
+		DecLastPosition decLastPosition2 = new DecLastPosition(2L, "神经科学", 1, null, 12);
+		decLastPositionService.addDecLastPosition(decLastPosition2);
+		DecLastPosition decLastPosition3 = new DecLastPosition(5L, "人格心理学", 1, null, null);
+		decLastPositionService.addDecLastPosition(decLastPosition3);
+		return decLastPosition3;
 	}
 }
