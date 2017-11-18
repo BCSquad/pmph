b0c327d1e2f7ffcf163b01a64bdfa7372783276e
lyc <513760648@qq.com>
2017-11-18 17:44:40 +0800

    修改不符合规范的方法名，并在添加房中增加必要的判断条件  by lyc

--- src/main/java/com/bc/pmpheep/back/service/TextbookServiceImpl.java
+++ src/main/java/com/bc/pmpheep/back/service/TextbookServiceImpl.java
@@ -33,6 +33,10 @@ public class TextbookServiceImpl implements TextbookService {
      */
     @Override
     public Textbook addTextbook(Textbook textbook) throws CheckedServiceException {
+    	if (ObjectUtil.isNull(textbook.getMaterialId())){
+    		throw new CheckedServiceException(CheckedExceptionBusiness.TEXTBOOK,
+    				CheckedExceptionResult.NULL_PARAM, "教材id不能为空！");
+    	}
         if (StringUtil.isEmpty(textbook.getTextbookName())) {
             throw new CheckedServiceException(CheckedExceptionBusiness.TEXTBOOK,
                                               CheckedExceptionResult.NULL_PARAM, "书籍名称为空");
@@ -44,6 +48,10 @@ public class TextbookServiceImpl implements TextbookService {
         if (ObjectUtil.isNull(textbook.getSort())) {
             throw new CheckedServiceException(CheckedExceptionBusiness.TEXTBOOK,
                                               CheckedExceptionResult.NULL_PARAM, "图书序号为空");
+        }
+        if (ObjectUtil.isNull(textbook.getFounderId())){
+        	throw new CheckedServiceException(CheckedExceptionBusiness.TEXTBOOK,
+        			CheckedExceptionResult.NULL_PARAM, "创建人id不能为空！");
         }
         textbookDao.addTextbook(textbook);
         return textbook;
@@ -94,12 +102,12 @@ public class TextbookServiceImpl implements TextbookService {
     }
 
     @Override
-    public List<Textbook> getTextBookByMaterialId(Long materialId) throws CheckedServiceException {
+    public List<Textbook> getTextbookByMaterialId(Long materialId) throws CheckedServiceException {
         if (ObjectUtil.isNull(materialId)) {
             throw new CheckedServiceException(CheckedExceptionBusiness.TEXTBOOK,
                                               CheckedExceptionResult.NULL_PARAM, "教材ID为空，禁止查询！");
         }
-        return textbookDao.getTextBookByMaterialId(materialId);
+        return textbookDao.getTextbookByMaterialId(materialId);
     }
 
 }
