50a599713e2ed99af248fe42dc958b0caa9a62a1
wuyun <wuyun@wuyun_pc>
2017-11-22 13:43:52 +0800

    修改路由地址

--- src/main/java/com/bc/pmpheep/back/controller/material/MaterialController.java
+++ src/main/java/com/bc/pmpheep/back/controller/material/MaterialController.java
@@ -74,7 +74,7 @@ public class MaterialController {
 	 * @return
 	 */
 	@LogDetail(businessType = Business_Type, logRemark = "新建遴选公告")
-	@RequestMapping(value = "/add/material", method = RequestMethod.POST)
+	@RequestMapping(value = "/new/material", method = RequestMethod.POST)
 	public ResponseBean material(
 			MaterialVO materialVO,
 			HttpServletRequest request, 
