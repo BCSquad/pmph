79dd0aca74241f189e06879e414a374011c0ec8b
wuyun <wuyun@wuyun_pc>
2017-12-21 09:48:28 +0800

    冲突了

--- src/main/java/com/bc/pmpheep/back/util/Const.java
+++ src/main/java/com/bc/pmpheep/back/util/Const.java
@@ -40,7 +40,8 @@ public class Const {
     public static final boolean      ISROLLBACK                    = true;
     // 用户SessionId
     public static final String       USER_SEESION_ID               = "userSessionId";
-    public static final String       SESSION_ID                    = "sessionId";
+    //public static final String       SESSION_ID                    = "sessionId";
+    public static final String       SESSION_ID                    = "JSESSIONID";
     // PMPH_USER_TOKEN
     public static final String       SEESION_PMPH_USER_TOKEN       = "sessionPmphUserToken";
     // WRITER_USER_TOKEN
