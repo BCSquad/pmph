f8de56a2de584e9d29c113cb578f0c326410b193
nyz526 <nyz526@163.com>
2017-12-21 16:57:00 +0800

    问卷调查service提交

--- src/main/java/com/bc/pmpheep/back/dao/SurveyQuestionDao.java
+++ src/main/java/com/bc/pmpheep/back/dao/SurveyQuestionDao.java
@@ -1,8 +1,12 @@
 package com.bc.pmpheep.back.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import com.bc.pmpheep.back.po.SurveyQuestion;
+import com.bc.pmpheep.back.vo.SurveyQuestionOptionCategoryVO;
 
 /**
  * SurveyQuestion问题实体类数据访问层接口
@@ -51,4 +55,28 @@ public interface SurveyQuestionDao {
      * @return 影响行数
      */
     SurveyQuestion selectSurveyQuestionById(Long id);
+
+    /**
+     * 
+     * <pre>
+     * 功能描述：根据问题id查询问题选项
+     * 使用示范：
+     *
+     * @param id Question主键ID
+     * @return
+     * </pre>
+     */
+    /**
+     * 
+     * <pre>
+     * 功能描述：根据问题id/问题类型Id查询问题选项
+     * 使用示范：
+     *
+     * @param questionId Question主键ID
+     * @param categoryId Question_Category主键ID
+     * @return
+     * </pre>
+     */
+    List<SurveyQuestionOptionCategoryVO> getQuestionOptionByQuestionIdOrCategoryId(
+    @Param("questionId") Long questionId, @Param("categoryId") Long categoryId);
 }
