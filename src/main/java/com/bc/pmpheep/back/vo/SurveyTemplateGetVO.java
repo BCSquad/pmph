af4b5d3875554c71364d41054e4f0d579c22892f
douniwanxiaotao <244769619@qq.com>
2017-12-22 09:38:55 +0800

    新增通过模版id获取问卷表标题和简介接口，VO

---
+++ src/main/java/com/bc/pmpheep/back/vo/SurveyTemplateGetVO.java
@@ -0,0 +1,79 @@
+package com.bc.pmpheep.back.vo;
+
+import org.apache.ibatis.type.Alias;
+
+/**
+ * SurveyTemplate模版实体类
+ * 
+ * @author tyc
+ */
+@Alias("SurveyTemplateGetVO")
+public class SurveyTemplateGetVO implements java.io.Serializable {
+
+    private static final long serialVersionUID = -7095331687874274350L;
+    /**
+     * 主键
+     */
+    private Long id;
+    /**
+     * 问卷标题
+     */
+    private String title;
+    /**
+     * 问卷副标题
+     */
+    private String subhead;
+    /**
+     * 问卷简介
+     */
+    private String intro;
+
+	public SurveyTemplateGetVO() {
+    }
+
+    public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+
+	public String getSubhead() {
+		return subhead;
+	}
+
+
+	public void setSubhead(String subhead) {
+		this.subhead = subhead;
+	}
+
+
+	public String getIntro() {
+		return intro;
+	}
+
+
+	public void setIntro(String intro) {
+		this.intro = intro;
+	}
+
+	@Override
+	public String toString() {
+		return "SurveyTemplateGetVO [id=" + id + ", title=" + title
+				+ ", subhead=" + subhead + ", intro=" + intro + "]";
+	}
+
+ 
+}
