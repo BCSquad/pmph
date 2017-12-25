5c0c905ee4e113242d53aa879bb55adf00815672
nyz526 <nyz526@163.com>
2017-12-25 17:18:37 +0800

    问卷调查代码提交

--- src/main/java/com/bc/pmpheep/back/util/mail/JavaMailSenderUtil.java
+++ src/main/java/com/bc/pmpheep/back/util/mail/JavaMailSenderUtil.java
@@ -8,7 +8,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.stereotype.Component;
 
 /**
  * 
@@ -27,7 +26,6 @@ import org.springframework.stereotype.Component;
  * @审核人 ：
  * </pre>
  */
-@Component
 public class JavaMailSenderUtil {
     Logger                            logger     =
                                                  LoggerFactory.getLogger(JavaMailSenderUtil.class);
