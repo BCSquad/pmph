78263e17c9748dc9d9f88727b1099e75e80c7725
Administrator <Administrator@mr>
2017-12-29 17:45:46 +0800

    增加积分管理控制层

---
+++ src/main/java/com/bc/pmpheep/back/controller/writerpoint/WriterPointController.java
@@ -0,0 +1,16 @@
+package com.bc.pmpheep.back.controller.writerpoint;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 积分管理用户积分控制层
+ * @author mr
+ * 2017-12-29
+ */
+@Controller
+@RequestMapping()
+@SuppressWarnings({ "rawtypes", "unchecked" })
+public class WriterPointController {
+	
+}
