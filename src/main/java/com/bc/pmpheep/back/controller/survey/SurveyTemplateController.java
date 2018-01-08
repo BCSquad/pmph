cfcdb94b3cd8c89c0ce4676b219494f78dd3ee5d
nyz526 <nyz526@163.com>
2018-01-08 16:41:40 +0800

    问卷修改SessionId

--- src/main/java/com/bc/pmpheep/back/controller/survey/SurveyTemplateController.java
+++ src/main/java/com/bc/pmpheep/back/controller/survey/SurveyTemplateController.java
@@ -88,10 +88,9 @@ public class SurveyTemplateController {
     public ResponseBean create(@RequestParam("questionAnswerJosn") String questionAnswerJosn,
     SurveyTemplateVO surveyTemplateVO, HttpServletRequest request) {
         String sessionId = CookiesUtil.getSessionId(request);
-        return new ResponseBean(
-                                surveyTemplateService.addSurveyTemplateVO(questionAnswerJosn,
+        return new ResponseBean(surveyTemplateService.addSurveyTemplateVO(questionAnswerJosn,
                                                                           surveyTemplateVO,
-                                                                          "8F186001FB4243B96B1E42A766338D40"));
+                                                                          sessionId));
     }
 
     /**
