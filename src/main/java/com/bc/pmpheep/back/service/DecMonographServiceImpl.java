a28a4b73941ac1a94320632f1c46431ac8e5b9a4
douniwanxiaotao <244769619@qq.com>
2018-01-17 10:02:17 +0800

    添加注解

--- src/main/java/com/bc/pmpheep/back/service/DecMonographServiceImpl.java
+++ src/main/java/com/bc/pmpheep/back/service/DecMonographServiceImpl.java
@@ -1,6 +1,7 @@
 package com.bc.pmpheep.back.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.bc.pmpheep.back.dao.DecMonographDao;
 import com.bc.pmpheep.back.po.DecMonograph;
@@ -14,6 +15,7 @@ import com.bc.pmpheep.service.exception.CheckedServiceException;
  * @author tyc
  * 2018年1月16日 09:51
  */
+@Service
 public class DecMonographServiceImpl implements DecMonographService{
 
 	@Autowired
