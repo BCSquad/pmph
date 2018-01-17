2139c0fb39948a6e2de16f050edc06e8b124b835
zqf050217 <791038935@qq.com>
2018-01-17 15:32:57 +0800

    提交作家动态

--- src/main/java/com/bc/pmpheep/back/service/TopicServiceImpl.java
+++ src/main/java/com/bc/pmpheep/back/service/TopicServiceImpl.java
@@ -63,6 +63,8 @@ public class TopicServiceImpl implements TopicService {
 	TopicExtraService topicExtraService;
 	@Autowired
 	TopicWriertService topicWriertService;
+	@Autowired
+	WriterUserTrendstService writerUserTrendstService;
 
 	@Override
 	public PageResult<TopicOPtsManagerVO> listOpts(String sessionId, PageParameter<TopicOPtsManagerVO> pageParameter)
@@ -244,12 +246,12 @@ public class TopicServiceImpl implements TopicService {
 			topicLogService.add(topicLog);
 			result = "SUCCESS";
 		}
+		WriterUserTrendst writerUserTrendst = new WriterUserTrendst();
+		writerUserTrendst.setUserId(topicDao.getTopicTextVO(topic.getId()).getUserId());
+		writerUserTrendst.setType(0);
 		if (ObjectUtil.notNull(topic.getAuthProgress())) {
 			if (3 == topic.getAuthProgress()) {
-				WriterUserTrendst writerUserTrendst = new WriterUserTrendst();
-				writerUserTrendst.setUserId(topicDao.getTopicTextVO(topic.getId()).getUserId());
-				writerUserTrendst.setType(0);
-				writerUserTrendst.setDetail("");
+				writerUserTrendst.setDetail("{title:\"" + CheckedExceptionBusiness.TOPIC + "\",content:\"您的选题已经通过。\"}");
 				// 创建本版号并将本版号放入数据中
 				String editionnum = "10" + new SimpleDateFormat("yyyy").format(new Date());
 				String vn = topicDao.getMaxTopicVn();
@@ -277,7 +279,10 @@ public class TopicServiceImpl implements TopicService {
 						+ topicTextVO.getOriginalAuthor() + "','" + topicTextVO.getNation() + "','','"
 						+ topicTextVO.getEdition() + "','','11','" + remark + "',GETDATE(),1)";
 				SqlHelper.executeUpdate(sql, null);
+			} else {
+				writerUserTrendst.setDetail("{title:\"" + CheckedExceptionBusiness.TOPIC + "\",content:\"您的选题没有通过。\"}");
 			}
+			writerUserTrendstService.addWriterUserTrendst(writerUserTrendst);
 		}
 		return result;
 	}
