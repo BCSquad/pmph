daf9871fcaf04ca965a12c13256d61abba3a8073
zqf050217 <791038935@qq.com>
2018-01-19 15:14:57 +0800

    修复图书isbn不能查询的问题 添加作家动态img属性

--- src/main/java/com/bc/pmpheep/back/service/TopicServiceImpl.java
+++ src/main/java/com/bc/pmpheep/back/service/TopicServiceImpl.java
@@ -251,7 +251,8 @@ public class TopicServiceImpl implements TopicService {
 		writerUserTrendst.setType(0);
 		if (ObjectUtil.notNull(topic.getAuthProgress())) {
 			if (3 == topic.getAuthProgress()) {
-				writerUserTrendst.setDetail("{title:\"" + CheckedExceptionBusiness.TOPIC + "\",content:\"您的选题已经通过。\"}");
+				writerUserTrendst.setDetail(
+						"{title:\"" + CheckedExceptionBusiness.TOPIC + "\",content:\"您的选题已经通过。\",img:\"1\"}");
 				// 创建本版号并将本版号放入数据中
 				String editionnum = "10" + new SimpleDateFormat("yyyy").format(new Date());
 				String vn = topicDao.getMaxTopicVn();
@@ -280,7 +281,8 @@ public class TopicServiceImpl implements TopicService {
 						+ topicTextVO.getEdition() + "','','11','" + remark + "',GETDATE(),1)";
 				SqlHelper.executeUpdate(sql, null);
 			} else {
-				writerUserTrendst.setDetail("{title:\"" + CheckedExceptionBusiness.TOPIC + "\",content:\"您的选题没有通过。\"}");
+				writerUserTrendst.setDetail(
+						"{title:\"" + CheckedExceptionBusiness.TOPIC + "\",content:\"您的选题没有通过。\",img:\"2\"}");
 			}
 			writerUserTrendstService.addWriterUserTrendst(writerUserTrendst);
 		}
