5349af7247601e3e98f6950c0c3167d87f82ce9e
lyc <513760648@qq.com>
2018-02-09 17:47:17 +0800

    修改作家用户职称为数字的情况   by lyc

--- src/main/java/com/bc/pmpheep/migration/MigrationStageOne.java
+++ src/main/java/com/bc/pmpheep/migration/MigrationStageOne.java
@@ -336,6 +336,30 @@ public class MigrationStageOne {
             }
             String position = (String) map.get("duties");
             String title = (String) map.get("positional");
+            if (StringUtil.isEmpty(title)){
+            	title = "-";
+            }
+            if (!"-".equals(title)){
+	            switch (title) {
+				case "1097":
+					title = "教授";
+					break;
+				case "1098":
+					title = "副教授";
+	                break;
+				case "1099":
+					title = "讲师";
+					break;
+				case "1100":
+					title = "正高";
+					break;
+				case "1101":
+					title = "副高";
+					break;
+				default:
+					title = "其他";
+				}
+            }
             String fax = (String) map.get("fax");
             String handphone = (String) map.get("handset");
             if (StringUtil.notEmpty(handphone) && "null".equals(handphone)){
