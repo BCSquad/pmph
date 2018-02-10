8b3de04a71e12f1442b7af67962392d22b3bc423
Administrator <Administrator@mr>
2018-02-10 17:42:34 +0800

    爬虫增加下载图片的方式

--- src/main/java/com/bc/pmpheep/general/runnable/Download.java
+++ src/main/java/com/bc/pmpheep/general/runnable/Download.java
@@ -1,9 +1,13 @@
 package com.bc.pmpheep.general.runnable;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -18,6 +22,7 @@ import javax.net.ssl.TrustManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.bc.pmpheep.back.util.FileUtil;
 import com.bc.pmpheep.general.bean.FileType;
 import com.bc.pmpheep.general.service.FileService;
 
@@ -111,4 +116,47 @@ public class Download {
 		}
 		return listHtmlImgs; 
 	}
+	public void download(String urlString, String filename, String savePath) throws Exception {
+        InputStream is = null;
+        OutputStream os = null;
+        List<String> listHtmlImgs = new ArrayList<String>();
+        try {
+            // 构造URL
+            URL url = new URL(urlString);
+            // 打开连接
+            URLConnection con = url.openConnection();
+            // 设置请求超时为5s
+            con.setConnectTimeout(5 * 1000);
+            // 输入流
+            is = con.getInputStream();
+
+            // 1K的数据缓冲
+            byte[] bs = new byte[1024];
+            // 读取到的数据长度
+            int len;
+            // 输出的文件流
+            File sf = new File(savePath);
+            if (!sf.exists()) {
+                sf.mkdirs();
+            }
+            os = new FileOutputStream(sf.getPath() + "\\" + filename);
+            // 开始读取
+            while ((len = is.read(bs)) != -1) {
+                os.write(bs, 0, len);
+            }
+            File file = new File("D://2.png");
+            Random rand = new Random();
+            String mongoId =fileService.saveLocalFile(file, FileType.CMS_IMG, (long) rand.nextInt(900) + 100);
+            listHtmlImgs.add(mongoId);
+            FileUtil.deleteDirectory("D:\\\\");
+            System.out.println(mongoId);
+        } catch (Exception e) {
+        	e.printStackTrace();
+        } finally {
+            // 完毕，关闭所有链接
+            os.close();
+            is.close();
+        }
+    }
+   
 }
