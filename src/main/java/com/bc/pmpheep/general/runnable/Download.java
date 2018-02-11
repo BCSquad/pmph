80c86d21f7eb93461fb7933fd99a856d2a37e40f
Administrator <Administrator@mr>
2018-02-11 10:36:55 +0800

    修改爬虫图片下载的方法

--- src/main/java/com/bc/pmpheep/general/runnable/Download.java
+++ src/main/java/com/bc/pmpheep/general/runnable/Download.java
@@ -101,47 +101,59 @@ public class Download {
 		}
 		return listHtmlImgs; 
 	}
-	public void download(String urlString, String filename, String savePath) throws Exception {
+	
+	/**
+	 * 下载图片
+	 * @param listImgSrc
+	 * 			图片url集合
+	 * @return	返回芒果id集合
+	 * @throws Exception
+	 */
+	public List<String> download(List<String> listImgSrc) throws Exception {
         InputStream is = null;
         OutputStream os = null;
+        String savePath = null;
+        String filename = null;
         List<String> listHtmlImgs = new ArrayList<String>();
-        try {
-            // 构造URL
-            URL url = new URL(urlString);
-            // 打开连接
-            URLConnection con = url.openConnection();
-            // 设置请求超时为5s
-            con.setConnectTimeout(5 * 1000);
-            // 输入流
-            is = con.getInputStream();
-
-            // 1K的数据缓冲
-            byte[] bs = new byte[1024];
-            // 读取到的数据长度
-            int len;
-            // 输出的文件流
-            File sf = new File(savePath);
-            if (!sf.exists()) {
-                sf.mkdirs();
-            }
-            os = new FileOutputStream(sf.getPath() + "\\" + filename);
-            // 开始读取
-            while ((len = is.read(bs)) != -1) {
-                os.write(bs, 0, len);
-            }
-            File file = new File("D://2.png");
-            Random rand = new Random();
-            String mongoId =fileService.saveLocalFile(file, FileType.CMS_IMG, (long) rand.nextInt(900) + 100);
-            listHtmlImgs.add(mongoId);
-            FileUtil.deleteDirectory("D:\\\\");
-            System.out.println(mongoId);
-        } catch (Exception e) {
-        	e.printStackTrace();
-        } finally {
-            // 完毕，关闭所有链接
-            os.close();
-            is.close();
-        }
+        for (String urlString : listImgSrc) {
+        	try {
+                 // 构造URL
+                 URL url = new URL(urlString);
+                 // 打开连接
+                 URLConnection con = url.openConnection();
+                 // 设置请求超时为5s
+                 con.setConnectTimeout(5 * 1000);
+                 // 输入流
+                 is = con.getInputStream();
+                 // 1K的数据缓冲
+                 byte[] bs = new byte[1024];
+                 // 读取到的数据长度
+                 int len;
+                 // 输出的文件流
+                 File sf = new File(savePath);
+                 if (!sf.exists()) {
+                     sf.mkdirs();
+                 }
+                 os = new FileOutputStream(sf.getPath() + "\\" + filename);
+                 // 开始读取
+                 while ((len = is.read(bs)) != -1) {
+                     os.write(bs, 0, len);
+                 }
+                 File file = new File("D://2.png");
+                 Random rand = new Random();
+                 String mongoId =fileService.saveLocalFile(file, FileType.CMS_IMG, (long) rand.nextInt(900) + 100);
+                 listHtmlImgs.add(mongoId);
+                 FileUtil.deleteDirectory("D:\\\\");
+                 System.out.println(mongoId);
+             } catch (Exception e) {
+             	e.printStackTrace();
+             } finally {
+                 // 完毕，关闭所有链接
+                 os.close();
+                 is.close();
+             }
+		}
+		return listHtmlImgs;
     }
    
 }
