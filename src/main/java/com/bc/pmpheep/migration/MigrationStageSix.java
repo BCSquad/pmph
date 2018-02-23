8b1945bdd58a50543d8713ad94f10bb7591e0704
douniwanxiaotao <244769619@qq.com>
2018-02-23 17:53:45 +0800

    修改数据迁移图六

--- src/main/java/com/bc/pmpheep/migration/MigrationStageSix.java
+++ src/main/java/com/bc/pmpheep/migration/MigrationStageSix.java
@@ -112,7 +112,7 @@ public class MigrationStageSix {
         }
         Date begin = new Date();
         declaration();
-        decEduExp();
+        /*decEduExp();
         decWorkExp();
         decTeachExp();
         decAcade();
@@ -124,7 +124,7 @@ public class MigrationStageSix {
         decResearch();
         decExtension();
         decPosition();
-        decPositionPublished();
+        decPositionPublished();*/
         logger.info("迁移第六步运行结束，用时：{}", JdbcHelper.getPastTime(begin));
     }
 
@@ -243,10 +243,7 @@ public class MigrationStageSix {
 			}
             if (StringUtil.isEmpty(realName) && isStagingJudge.intValue() == 0) { // 申报表作家姓名为空并且不暂存
             	map.put(SQLParameters.EXCEL_EX_HEADER, sb.append("找到申报表作家姓名为空。"));
-                excel.add(map);
-                logger.debug("找到申报表作家姓名为空，此结果将被记录在Excel中");
                 realNameCount++;
-                continue;
             } else {
             	declaration.setRealname(realName);
 			}
