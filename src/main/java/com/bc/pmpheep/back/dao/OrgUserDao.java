adeac2f72ac7d4960755b4c3bdda406b27ca49ae
nyz526 <nyz526@163.com>
2018-03-05 17:35:29 +0800

    添加机构用户查询

--- src/main/java/com/bc/pmpheep/back/dao/OrgUserDao.java
+++ src/main/java/com/bc/pmpheep/back/dao/OrgUserDao.java
@@ -12,130 +12,139 @@ import com.bc.pmpheep.back.vo.OrgAndOrgUserVO;
  * @author mryang
  */
 public interface OrgUserDao {
-	/**
-	 * 根据机构id集查询用户 (逻辑没有删除和启用的)
-	 */
-	List<OrgUser> getOrgUserListByOrgIds(List<Long> orgIds);
+    /**
+     * 根据机构id集查询用户 (逻辑没有删除和启用的)
+     */
+    List<OrgUser> getOrgUserListByOrgIds(List<Long> orgIds);
 
-	/**
-	 * 根据机构id获取机构管理员
-	 */
-	OrgUser getOrgUserByOrgId(Long orgId);
+    /**
+     * 根据机构id获取机构管理员
+     */
+    OrgUser getOrgUserByOrgId(Long orgId);
 
-	/**
-	 * 
-	 * @param orgUser
-	 *            实体对象
-	 * @return 影响行数
-	 */
-	Integer addOrgUser(OrgUser orgUser);
+    /**
+     * 
+     * @param orgUser 实体对象
+     * @return 影响行数
+     */
+    Integer addOrgUser(OrgUser orgUser);
 
-	/**
-	 * 根据主键Id查询对象
-	 * 
-	 * @param id
-	 *            主键id
-	 * @return OrgUser OrgUser对象
-	 */
-	OrgUser getOrgUserById(Long id);
+    /**
+     * 根据主键Id查询对象
+     * 
+     * @param id 主键id
+     * @return OrgUser OrgUser对象
+     */
+    OrgUser getOrgUserById(Long id);
 
-	/**
-	 * 
-	 * <pre>
+    /**
+     * 
+     * <pre>
 	 * 功能描述：根据主键Id集合查询对象集合
 	 * 使用示范：
 	 *
 	 * &#64;param ids 主键id 集合
 	 * &#64;return
 	 * </pre>
-	 */
-	List<OrgUser> getOrgUserByIds(List<Long> ids);
+     */
+    List<OrgUser> getOrgUserByIds(List<Long> ids);
 
-	/**
-	 * 
-	 * @param id
-	 * @return 影响行数
-	 */
-	Integer deleteOrgUserById(Long id);
+    /**
+     * 
+     * @param id
+     * @return 影响行数
+     */
+    Integer deleteOrgUserById(Long id);
 
-	/**
-	 * @param orgUser
-	 * @return 影响行数
-	 */
-	Integer updateOrgUser(OrgUser orgUser);
+    /**
+     * @param orgUser
+     * @return 影响行数
+     */
+    Integer updateOrgUser(OrgUser orgUser);
 
-	/**
-	 * 
-	 * <pre>
+    /**
+     * 
+     * <pre>
 	 * 功能描述：查询表单总条数
 	 * 使用示范：
 	 *
 	 * &#64;return 表单的总条数
 	 * </pre>
-	 */
-	Long getOrgUserCount();
+     */
+    Long getOrgUserCount();
 
-	/**
-	 * 
-	 * <pre>
+    /**
+     * 
+     * <pre>
 	 * 功能描述：分页查询作家用户
 	 * 使用示范：
 	 *
 	 * &#64;param pageParameter 传入的查询条件
 	 * &#64;return 需要的作家用户集合
 	 * </pre>
-	 */
-	List<OrgAndOrgUserVO> getListOrgUser(PageParameter<OrgAndOrgUserVO> pageParameter);
+     */
+    List<OrgAndOrgUserVO> getListOrgUser(PageParameter<OrgAndOrgUserVO> pageParameter);
 
-	/**
-	 * 
-	 * <pre>
+    /**
+     * 
+     * <pre>
 	 * 功能描述： 查询总共的条数
 	 * 使用示范：
 	 *
 	 * &#64;param pageParameter 传入查询条件
 	 * &#64;return 查询到的条数
 	 * </pre>
-	 */
-	Integer getListOrgUserTotal(PageParameter<OrgAndOrgUserVO> pageParameter);
+     */
+    Integer getListOrgUserTotal(PageParameter<OrgAndOrgUserVO> pageParameter);
 
-	/**
-	 * 
-	 * <pre>
+    /**
+     * 
+     * <pre>
 	 * 功能描述：学校管理员审核(按Id更新审核状态)
 	 * 使用示范：
 	 *
 	 * &#64;param orgUser
 	 * &#64;return
 	 * </pre>
-	 */
-	Integer updateOrgUserProgressById(List<OrgUser> orgUser);
+     */
+    Integer updateOrgUserProgressById(List<OrgUser> orgUser);
 
-	/**
-	 * 
-	 * 
-	 * 功能描述：通过机构id获取机构用户
-	 * 
-	 * @param orgId
-	 *            机构id
-	 * @return
-	 * 
-	 */
-	List<OrgUser> listOrgUserByOrgId(Long orgId);
+    /**
+     * 
+     * 
+     * 功能描述：通过机构id获取机构用户
+     * 
+     * @param orgId 机构id
+     * @return
+     * 
+     */
+    List<OrgUser> listOrgUserByOrgId(Long orgId);
 
-	/**
-	 * 根据username查询是否存在该机构名称
-	 * 
-	 * @param username
-	 * @return
-	 */
-	List<OrgUser> getOrgUsername(String username);
+    /**
+     * 根据username查询是否存在该机构名称
+     * 
+     * @param username
+     * @return
+     */
+    List<OrgUser> getOrgUsername(String username);
 
-	/**
-	 * 查询机构认证总数
-	 * 
-	 * @return
-	 */
-	Integer geCount();
+    /**
+     * 查询机构认证总数
+     * 
+     * @return
+     */
+    Integer geCount();
+
+    /**
+     * 
+     * <pre>
+     * 功能描述：分页查询作家用户
+     * 使用示范：
+     *
+     * &#64;param pageParameter 传入的查询条件
+     * &#64;return 需要的作家用户集合
+     * </pre>
+     */
+    List<OrgAndOrgUserVO> getListAllOrgUser(PageParameter<OrgAndOrgUserVO> pageParameter);
 
 }
--- src/main/java/com/bc/pmpheep/back/service/OrgUserService.java
+++ src/main/java/com/bc/pmpheep/back/service/OrgUserService.java
@@ -16,58 +16,57 @@ import com.bc.pmpheep.service.exception.CheckedServiceException;
  * @author Mryang
  */
 public interface OrgUserService {
-	/**
-	 * 根据机构id集查询用户(逻辑没有删除和启用的)
-	 */
-	List<OrgUser> getOrgUserListByOrgIds(List<Long> orgIds) throws CheckedServiceException;
+    /**
+     * 根据机构id集查询用户(逻辑没有删除和启用的)
+     */
+    List<OrgUser> getOrgUserListByOrgIds(List<Long> orgIds) throws CheckedServiceException;
 
-	/**
-	 * 
-	 * 
-	 * 功能描述：根据机构id获取机构管理员
-	 *
-	 * @param orgId
-	 * @return
-	 * @throws CheckedServiceException
-	 *
-	 */
-	OrgUser getOrgUserByOrgId(Long orgId) throws CheckedServiceException;
+    /**
+     * 
+     * 
+     * 功能描述：根据机构id获取机构管理员
+     * 
+     * @param orgId
+     * @return
+     * @throws CheckedServiceException
+     * 
+     */
+    OrgUser getOrgUserByOrgId(Long orgId) throws CheckedServiceException;
 
-	/**
-	 * 
-	 * @param orgUser
-	 *            实体对象
-	 * @return 带主键的 OrgUser
-	 * @throws CheckedServiceException
-	 */
-	OrgUser addOrgUser(OrgUser orgUser) throws CheckedServiceException;
+    /**
+     * 
+     * @param orgUser 实体对象
+     * @return 带主键的 OrgUser
+     * @throws CheckedServiceException
+     */
+    OrgUser addOrgUser(OrgUser orgUser) throws CheckedServiceException;
 
-	/**
-	 * 
-	 * @param id
-	 * @return OrgUser
-	 * @throws CheckedServiceException
-	 */
-	OrgUser getOrgUserById(Long id) throws CheckedServiceException;
+    /**
+     * 
+     * @param id
+     * @return OrgUser
+     * @throws CheckedServiceException
+     */
+    OrgUser getOrgUserById(Long id) throws CheckedServiceException;
 
-	/**
-	 * 
-	 * @param id
-	 * @return 影响行数
-	 * @throws CheckedServiceException
-	 */
-	Integer deleteOrgUserById(Long id) throws CheckedServiceException;
+    /**
+     * 
+     * @param id
+     * @return 影响行数
+     * @throws CheckedServiceException
+     */
+    Integer deleteOrgUserById(Long id) throws CheckedServiceException;
 
-	/**
-	 * @param orgUser
-	 * @return 影响行数
-	 * @throws CheckedServiceException
-	 */
-	Integer updateOrgUser(OrgUser orgUser) throws CheckedServiceException;
+    /**
+     * @param orgUser
+     * @return 影响行数
+     * @throws CheckedServiceException
+     */
+    Integer updateOrgUser(OrgUser orgUser) throws CheckedServiceException;
 
-	/**
-	 * 
-	 * <pre>
+    /**
+     * 
+     * <pre>
 	 * 功能描述：学校管理员审核(按Id更新审核状态)
 	 * 使用示范：
 	 *
@@ -76,61 +75,69 @@ public interface OrgUserService {
 	 * &#64;return
 	 * &#64;throws CheckedServiceException
 	 * </pre>
-	 */
-	Integer updateOrgUserProgressById(Integer progress, List<Long> orgUserIds) throws CheckedServiceException,IOException;
+     */
+    Integer updateOrgUserProgressById(Integer progress, List<Long> orgUserIds)
+    throws CheckedServiceException, IOException;
 
-	/**
-	 * 
-	 * 功能描述：分页查询作家用户
-	 * 
-	 * @param pageParameter
-	 *            传入的查询条件
-	 * @return
-	 */
-	PageResult<OrgAndOrgUserVO> getListOrgUser(PageParameter<OrgAndOrgUserVO> pageParameter)
-			throws CheckedServiceException;
+    /**
+     * 
+     * 功能描述：分页查询作家用户
+     * 
+     * @param pageParameter 传入的查询条件
+     * @return
+     */
+    PageResult<OrgAndOrgUserVO> getListOrgUser(PageParameter<OrgAndOrgUserVO> pageParameter)
+    throws CheckedServiceException;
 
-	/**
-	 * 
-	 * 
-	 * 功能描述：后台机构用户管理页面添加机构用户
-	 * 
-	 * @param OrgUser
-	 *            添加的机构用户用户属性
-	 * @return 是否成功
-	 * @throws CheckedServiceException
-	 * 
-	 */
-	String addOrgUserOfBack(OrgUser orgUser) throws CheckedServiceException;
+    /**
+     * 
+     * 
+     * 功能描述：后台机构用户管理页面添加机构用户
+     * 
+     * @param OrgUser 添加的机构用户用户属性
+     * @return 是否成功
+     * @throws CheckedServiceException
+     * 
+     */
+    String addOrgUserOfBack(OrgUser orgUser) throws CheckedServiceException;
 
-	/**
-	 * 
-	 * 
-	 * 功能描述：后台机构用户管理页面修改机构用户
-	 * 
-	 * @param writerUser
-	 *            修改机构用户用户的属性
-	 * @return 是否成功
-	 * @throws CheckedServiceException
-	 * 
-	 */
-	String updateOrgUserOfBack(OrgAndOrgUserVO orgAndOrgUserVO) throws CheckedServiceException;
+    /**
+     * 
+     * 
+     * 功能描述：后台机构用户管理页面修改机构用户
+     * 
+     * @param writerUser 修改机构用户用户的属性
+     * @return 是否成功
+     * @throws CheckedServiceException
+     * 
+     */
+    String updateOrgUserOfBack(OrgAndOrgUserVO orgAndOrgUserVO) throws CheckedServiceException;
 
-	/**
-	 * 功能描述：在机构用户页面增加机构用户
-	 * 
-	 * @param orgUser
-	 * @param org
-	 * @return 是否成功
-	 * @throws CheckedServiceException
-	 */
-	Object addOrgUserAndOrgOfBack(OrgUser orgUser, Org org) throws CheckedServiceException;
+    /**
+     * 功能描述：在机构用户页面增加机构用户
+     * 
+     * @param orgUser
+     * @param org
+     * @return 是否成功
+     * @throws CheckedServiceException
+     */
+    Object addOrgUserAndOrgOfBack(OrgUser orgUser, Org org) throws CheckedServiceException;
 
-	/**
-	 * 机构认证总数量
-	 * 
-	 * @param orgId
-	 * @return
-	 */
-	Integer getCount();
+    /**
+     * 机构认证总数量
+     * 
+     * @param orgId
+     * @return
+     */
+    Integer getCount();
+
+    /**
+     * 
+     * 功能描述：分页查询作家用户
+     * 
+     * @param pageParameter 传入的查询条件
+     * @return
+     */
+    PageResult<OrgAndOrgUserVO> getListAllOrgUser(PageParameter<OrgAndOrgUserVO> pageParameter)
+    throws CheckedServiceException;
 }
