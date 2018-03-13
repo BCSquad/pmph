8ed2a48dc39e10dc99f49be8f9cc5051fcf82098
nyz526 <nyz526@163.com>
2018-03-13 09:32:54 +0800

    内容管理代码优化

--- src/main/java/com/bc/pmpheep/back/service/CmsContentServiceImpl.java
+++ src/main/java/com/bc/pmpheep/back/service/CmsContentServiceImpl.java
@@ -558,23 +558,25 @@ public class CmsContentServiceImpl implements CmsContentService {
             }
         }
         resultMap.put("MaterialNoteAttachment", materialNoteAttachments);
-        // 文章封面图片
-        CmsExtra cmsExtra = cmsExtraService.getCmsExtraByAttachment(cmsContent.getCover());
-        String imgFileName = "默认封面.png";
-        String imgFilePath = RouteUtil.DEFAULT_USER_AVATAR;
-        if (ObjectUtil.notNull(cmsExtra)) {
-            imgFileName = cmsExtra.getAttachmentName();
-        } else {
-            GridFSDBFile file = fileService.get(cmsContent.getCover());
-            if (ObjectUtil.notNull(file)) {
-                imgFileName = file.getFilename();
+        if (Const.CMS_CATEGORY_ID_1.longValue() == cmsContent.getCategoryId().longValue()) {
+            // 文章封面图片
+            CmsExtra cmsExtra = cmsExtraService.getCmsExtraByAttachment(cmsContent.getCover());
+            String imgFileName = "默认封面.png";
+            String imgFilePath = RouteUtil.DEFAULT_USER_AVATAR;
+            if (ObjectUtil.notNull(cmsExtra)) {
+                imgFileName = cmsExtra.getAttachmentName();
+            } else {
+                GridFSDBFile file = fileService.get(cmsContent.getCover());
+                if (ObjectUtil.notNull(file)) {
+                    imgFileName = file.getFilename();
+                }
             }
+            resultMap.put("imgFileName", imgFileName);
+            if (!"DEFAULT".equals(cmsContent.getCover())) {
+                imgFilePath = cmsContent.getCover();
+            }
+            resultMap.put("imgFilePath", imgFilePath);
         }
-        resultMap.put("imgFileName", imgFileName);
-        if (!"DEFAULT".equals(cmsContent.getCover())) {
-            imgFilePath = cmsContent.getCover();
-        }
-        resultMap.put("imgFilePath", imgFilePath);
         return resultMap;
     }
 
