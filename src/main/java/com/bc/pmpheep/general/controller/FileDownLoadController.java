ef014c6c6c669cf02f578c6d46a678695952e1b0
yangliang <280530171@qq.com>
2018-03-13 17:13:51 +0800

    test fileDown

--- src/main/java/com/bc/pmpheep/general/controller/FileDownLoadController.java
+++ src/main/java/com/bc/pmpheep/general/controller/FileDownLoadController.java
@@ -688,8 +688,9 @@ public class FileDownLoadController {
         }
         String fileName = returnFileName(request, "纠错跟踪" + DateUtil.getTime() + ".xls");
         response.setCharacterEncoding("utf-8");
-        response.setContentType("application/force-download");
+        //response.setContentType("application/force-download");
         response.setHeader("Content-Disposition", "attachment;fileName=" + fileName);
+        response.setContentType("application/x-download");
         try (OutputStream out = response.getOutputStream()) {
             workbook.write(out);
             out.flush();
