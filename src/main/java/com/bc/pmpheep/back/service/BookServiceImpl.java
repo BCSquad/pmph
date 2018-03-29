73d104de29127f45166a23180a0da2bf81f12eec
zqf050217 <791038935@qq.com>
2018-03-29 08:47:58 +0800

    解决冲突

--- src/main/java/com/bc/pmpheep/back/service/BookServiceImpl.java
+++ src/main/java/com/bc/pmpheep/back/service/BookServiceImpl.java
@@ -197,6 +197,7 @@ public class BookServiceImpl extends BaseService implements BookService {
 				}
 			}
 			try {
+//				System.out.println("第"+(i+1)+"条数据，本版号为"+vns[i]+"  共"+vns.length+"条");
 				ot = PostBusyAPI(vns[i]);
 				if ("1".equals(ot.getJSONObject("RESP").getString("CODE"))) {
 					JSONArray array = ot.getJSONObject("RESP").getJSONObject("responseData").getJSONArray("results");
@@ -204,7 +205,6 @@ public class BookServiceImpl extends BaseService implements BookService {
 						Book book = BusyResJSONToModel(array.getJSONObject(0), null);
 						String content = book.getContent();// 获取到图书详情将其存入到图书详情表中
 						if (ObjectUtil.isNull(oldBook)) {
-							//System.out.println(book.getBookname());
 							book.setScore(9.0);
 							book.setType(1L);
 							bookDao.addBook(book);
