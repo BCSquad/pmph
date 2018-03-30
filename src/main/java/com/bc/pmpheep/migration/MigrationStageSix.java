bca10808daef488baa377a5f3087f54228c796f9
douniwanxiaotao <244769619@qq.com>
2018-03-30 20:23:41 +0800

    修改数据迁移图六——用户姓名问题

--- src/main/java/com/bc/pmpheep/migration/MigrationStageSix.java
+++ src/main/java/com/bc/pmpheep/migration/MigrationStageSix.java
@@ -301,11 +301,12 @@ public class MigrationStageSix {
                 logger.debug("找到申报表作家姓名为空并且没有暂存，此结果将被记录在Excel中");
                 continue;*/
             } else {
-            	if ("0579c3b70c3e4adcbd008b6065e83c7f".equals(id)) {
+            	/*if ("0579c3b70c3e4adcbd008b6065e83c7f".equals(id)) {
             		declaration.setRealname("魏敏杰");
             	} else {
-            		declaration.setRealname(realName);
-				}
+            		
+				}*/
+            	declaration.setRealname(realName);
             }
             if (StringUtil.isEmpty(sexJudge)) {
                 declaration.setSex(1);
