8b0690ef2567b0e49e2a529c57b952056a4b9000
evchang <572385791@qq.com>
2018-05-07 17:25:54 +0800

    修改进入小组聊天界面  查询登录人是否是小组成员

--- src/main/java/com/bc/pmpheep/back/controller/group/GroupController.java
+++ src/main/java/com/bc/pmpheep/back/controller/group/GroupController.java
@@ -430,13 +430,13 @@ public class GroupController {
 	@ResponseBody
 	@LogDetail(businessType = BUSSINESS_TYPE, logRemark = "获取历史消息")
 	@RequestMapping(value = "/list/message", method = RequestMethod.GET)
-	public ResponseBean message(Integer pageSize, Integer pageNumber, Long groupId, Long baseTime) {
+	public ResponseBean message(Integer pageSize, Integer pageNumber, Long groupId, Long baseTime,HttpServletRequest req) {
 		PageParameter<PmphGroupMessageVO> pageParameter = new PageParameter<>(pageNumber, pageSize);
 		PmphGroupMessageVO pmphGroupMessageVO = new PmphGroupMessageVO();
 		pmphGroupMessageVO.setGmtCreate(new Timestamp(baseTime));
 		pmphGroupMessageVO.setGroupId(groupId);
 		pageParameter.setParameter(pmphGroupMessageVO);
-		return new ResponseBean(pmphGroupMessageService.listPmphGroupMessage(pageParameter));
+		return new ResponseBean(pmphGroupMessageService.listPmphGroupMessage(pageParameter,req));
 	}
 
 	/**
