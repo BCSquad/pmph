2eae26a113e4e354f270adfbd12029ecb6febe26
zhangzhuo <651653676@qq.com>
2018-10-26 14:10:34 +0800

    1025 bug 修改

--- src/main/java/com/bc/pmpheep/back/service/ActivityManagementService.java
+++ src/main/java/com/bc/pmpheep/back/service/ActivityManagementService.java
@@ -13,13 +13,50 @@ import java.util.Map;
 
 public interface ActivityManagementService {
 
+    /**
+     * 查询列表
+     * @param pageParameter
+     * @param sessionId
+     * @return
+     */
     PageResult<ActivityVO> listActivity(PageParameter<ActivityVO> pageParameter, String sessionId);
-    Activity addActivity(Activity activity, String content, String sessionId, HttpServletRequest request);
-    Activity addActivity(Activity activity);
 
+    /**
+     * 添加活动,活动简介
+     * @param activity
+     * @param content
+     * @param sessionId
+     * @param request
+     * @return
+     */
+    Activity addActivity(Activity activity, String content, String sessionId, HttpServletRequest request);
+
+    /**
+     * 获取活动详情
+     * @param id
+     * @return
+     */
     Map<String, Object> getActivitDetailsyById(Long id);
 
+    /**
+     * 更新活动信息,与活动介绍
+     * @param activity
+     * @param content
+     * @param sessionId
+     * @param request
+     * @return
+     */
     Integer updateActivity(Activity activity, String content, String sessionId, HttpServletRequest request);
+
+    /**
+     * 更新状态
+     * @param activity
+     */
     void setActivityStatus(Activity activity);
+
+    /**
+     * 置顶
+     * @param activity
+     */
     void setActivitySetTop(Activity activity);
 }
--- src/main/java/com/bc/pmpheep/back/service/ActivityVideoService.java
+++ src/main/java/com/bc/pmpheep/back/service/ActivityVideoService.java
@@ -11,11 +11,38 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 public interface ActivityVideoService {
+
+    /**
+     * 获取视频列表
+     * @param pageParameter
+     * @param sessionId
+     * @return
+     */
     PageResult<ActivityVideoVO> listVideo(PageParameter<ActivityVideoVO> pageParameter, String sessionId);
+
+    /**
+     * 添加活动视频
+     * @param sessionId
+     * @param activityVideo
+     * @param cover
+     * @return
+     * @throws IOException
+     */
 
     Integer addActivityVideo(String sessionId, ActivityVideo activityVideo, MultipartFile cover) throws IOException;
 
+    /**
+     * 活动与活动视频关联
+     * @param activityId
+     * @param ids
+     * @return
+     */
     Integer addActivityVideoChain(String activityId, String[] ids);
 
+    /**
+     * 删除活动视频
+     * @param id
+     * @return
+     */
     Integer deleteVideoByIds(Long id);
 }
